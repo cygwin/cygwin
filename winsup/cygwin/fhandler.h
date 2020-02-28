3c504a2a0bf12106b730150f28a5d1c7b5559ec4
Corinna Vinschen <corinna@vinschen.de>
2020-02-28 13:21:05 +0100

    Cygwin: AF_UNIX: rework fixup_after_exec
    fhandler_socket_unix::fixup_after_exec incorrectly calls
    fhandler_socket_unix::fixup_after_fork with a NULL parent process
    handle.  Not only that calling DuplicateHandle with a NULL parent
    handle fails, but it's utterly wrong trying to duplicate the handles
    at all here.
    
    Rather just set some important values to NULL and reopen the shared
    memory region.  Create a fixup_helper method to call common code from
    fixup_after_fork and fixup_after_exec.
    
    Add comments to other invocations of fixup_after_fork with NULL
    handle to mark them as correct this way.
    
    Signed-off-by: Corinna Vinschen <corinna@vinschen.de>

--- winsup/cygwin/fhandler.h
+++ winsup/cygwin/fhandler.h
@@ -1080,6 +1080,7 @@ class fhandler_socket_unix : public fhandler_socket
   void fixup_after_fork (HANDLE parent);
   void fixup_after_exec ();
   void set_close_on_exec (bool val);
+  void fixup_helper ();
 
  public:
   fhandler_socket_unix ();
