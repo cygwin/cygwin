62f11a5a57043404719cc5d81f3d2ebbf8142b29
Corinna Vinschen <corinna@vinschen.de>
2023-01-16 22:25:42 +0100

    Cygwin: open_shared: don't reuse shared_locations parameter as output
    For ages, open_shared uses the shared_locations parameter as
    output to indicate if the mapping for a shared region has been
    created or just opened.  Split this into two parameters.  Use
    the shared_locations parameter as input only, return the creation
    state of the mapping in a bool reference parameter.
    
    Conflict: Simple collision due to missing backport of
    	  3721a756b0d8 ("Cygwin: console: Make the console accessible from other terminals.")
    Signed-off-by: Corinna Vinschen <corinna@vinschen.de>

--- winsup/cygwin/fhandler/console.cc
+++ winsup/cygwin/fhandler/console.cc
@@ -148,15 +148,14 @@ beep ()
 }
 
 fhandler_console::console_state *
-fhandler_console::open_shared_console (HWND hw, HANDLE& h, bool& create)
+fhandler_console::open_shared_console (HWND hw, HANDLE& h, bool& created)
 {
   wchar_t namebuf[(sizeof "XXXXXXXXXXXXXXXXXX-consNNNNNNNNNN")];
   __small_swprintf (namebuf, L"%S-cons%p", &cygheap->installation_key, hw);
 
-  shared_locations m = create ? SH_SHARED_CONSOLE : SH_JUSTOPEN;
+  shared_locations m = created ? SH_SHARED_CONSOLE : SH_JUSTOPEN;
   console_state *res = (console_state *)
-    open_shared (namebuf, 0, h, sizeof (*shared_console_info), &m);
-  create = m != SH_JUSTOPEN;
+    open_shared (namebuf, 0, h, sizeof (*shared_console_info), m, created);
   return res;
 }
 
