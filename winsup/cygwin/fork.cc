picolibc/picolibc

commit f02b22dcee17b7f533c99c59e48dfe0d58e2382e

* fork.cc (frok::child): Change order of cleanup prior to return. (fork): Save more of the stack. * gendef: Fix some comments. * sigproc.cc (wait_sig): Clarify debug output.

winsup/cygwin/fork.cc
@@ -244,15 +244,15 @@ frok::child (void *)
 
   ForceCloseHandle1 (fork_info->forker_finished, forker_finished);
 
-  _my_tls.fixup_after_fork ();
   sigproc_init ();
 
   pthread::atforkchild ();
   fixup_timers_after_fork ();
-  fixup_hooks_after_fork ();
   cygbench ("fork-child");
   ld_preload ();
+  fixup_hooks_after_fork ();
   cygwin_finished_initializing = true;
+  _my_tls.fixup_after_fork ();
   return 0;
 }
 
@@ -562,7 +562,10 @@ fork ()
   grouped.first_dll = NULL;
   grouped.load_dlls = 0;
 
+  int res;
   void *esp;
+  int ischild;
+
   __asm__ volatile ("movl %%esp,%0": "=r" (esp));
 
   myself->set_has_pgid_children ();
@@ -576,8 +579,7 @@ fork ()
     }
 
   sig_send (NULL, __SIGHOLD);
-  int res;
-  int ischild = setjmp (grouped.ch.jmp);
+  ischild = setjmp (grouped.ch.jmp);
   if (!ischild)
     res = grouped.parent (esp);
   else

winsup/cygwin/gendef
@@ -114,7 +114,7 @@ __sigfe:
 
 	.global	__sigbe
 	.stabs	"_sigbe:F(0,1)",36,0,0,__sigbe
-__sigbe: # return here after cygwin syscall
+__sigbe:						# return here after cygwin syscall
 	pushl	%edx
 	pushl	%ebx
 	pushl	%eax					# don't clobber
@@ -131,7 +131,7 @@ __sigbe: # return here after cygwin syscall
 	xadd	%eax,$tls::stackptr(%ebx)		#  and get pointer
 	xorl	%edx,%edx
 	xchgl	%edx,-4(%eax)				# get return address from signal stack
-	xchgl	%edx,8(%esp)				# restore ebx/real return address
+	xchgl	%edx,8(%esp)				# restore edx/real return address
 	decl	$tls::incyg(%ebx)
 	decl	$tls::stacklock(%ebx)			# release lock
 	popl	%eax

winsup/cygwin/sigproc.cc
@@ -1175,8 +1175,8 @@ wait_sig (VOID *)
 	proc_subproc (PROC_CLEARWAIT, 0);
       if (pack.wakeup)
 	{
+	  sigproc_printf ("signalling pack.wakeup %p", pack.wakeup);
 	  SetEvent (pack.wakeup);
-	  sigproc_printf ("signalled %p", pack.wakeup);
 	}
       if (pack.si.si_signo == __SIGEXIT)
 	break;

cygwin/cygwin

commit 779e46b5b3ee8d0250f084d1cf2d68c0483a521e

Cygwin: fork: Call pthread::atforkchild () after other initializations
Previously, the callback registered by pthread_atfork() was called
in pthread::atforkchild() before _my_tls.fixup_after_fork(). This
caused misbehaviour if the callback used TLS-related functions.
More specifically, cygwait() for a mutex at the beginning of
fhandler_fifo_pipe::raw_write() failed because the event handle
_my_tls.signal_arrived, which is used in cygwait() internally and
designed to be initialized in _cygtls::fixup_after_fork(), was not
yet initialized at that point. Due to this problem, subprocesses
of CMake (versions >= 3.29.x) sometimes failed after the commit
7ed9adb356df ("Cygwin: pipe: Switch pipe mode to blocking mode by
default, 2024-09-05"). This commit triggered the issue because it
introduced cygwait() for the mutex in fhandler_fifo_pipe::raw_write().

This patch moves the pthread::atforkchild() at the end of the fork::
child(), i.e. after all initializations for child process is finished.

The reason why the issue happens not always but sometimes:

The event handle signal_arrived was never properly initialized when
fhandler_fifo_pipe::raw_write() was called from the callback. As a
result, its value was merely copied from the parent process during
a fork() operation. Since the event signal_arrived was not created
as inheritable, the handle value was fundamentally invalid. Despite
this, the issue only occurred occasionally. This inconsistency was
due to the handle value often coinciding with other existing handles
because of its small value, such as 0x1ac. As evidence of this, in
many cases where the issue did not manifest, the signal_arrived
handle was not even an event handle.

Addresses: https://cygwin.com/pipermail/cygwin/2025-March/257800.html
Addresses: https://github.com/msys2/msys2-runtime/issues/272
Fixes: f02b22dcee17 ("* fork.cc (frok::child): Change order of cleanup prior to return.")
Reported-by: Christoph Reiter <reiter.christoph@gmail.com>

winsup/cygwin/fork.cc
@@ -187,7 +187,6 @@ frok::child (volatile char * volatile here)
 
   ForceCloseHandle1 (fork_info->forker_finished, forker_finished);
 
-  pthread::atforkchild ();
   cygbench ("fork-child");
   ld_preload ();
   fixup_hooks_after_fork ();
@@ -199,6 +198,7 @@ frok::child (volatile char * volatile here)
   CloseHandle (hParent);
   hParent = NULL;
   cygwin_finished_initializing = true;
+  pthread::atforkchild ();
   return 0;
 }
 

winsup/cygwin/release/3.6.1
@@ -31,3 +31,8 @@ Fixes:
 
 - Return EMFILE when opening /dev/ptmx too many times.
   Addresses: https://cygwin.com/pipermail/cygwin/2025-March/257786.html
+
+- Move pthread::atforkchild() at the end of fork::child(). This fixes
+  subprocess failure in cmake (>= 3.29.x).
+  Addresses: https://cygwin.com/pipermail/cygwin/2025-March/257800.html
+  Addresses: https://github.com/msys2/msys2-runtime/issues/272