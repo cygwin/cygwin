picolibc/picolibc

commit 3e8a7eb1a868e9aa03d55ef24bd43d83d89b5fed

sys/unistd.h: fix definition of setproctitle_init
setproctitle_init is defined in c2x manner, omitting names for
the unused parameters.  This can result in warnings or errors
on certain compiler versions:

clang 8:

  error: parameter name omitted

clang 15:

  warning: omitting the parameter name in a function definition is a C2x
  extension [-Wc2x-extensions]

gcc -Wsystem-headers -pedantic -std=c17:

  warning: ISO C does not support omitting parameter names in function
  definitions before C2X [-Wpedantic]

Add parameters to avoid above warning and errors.

Fixes: 2e7f7b96e5f1 ("Cygwin: implement setproctitle")
Signed-off-by: Corinna Vinschen <corinna@vinschen.de>

newlib/libc/include/sys/unistd.h
@@ -215,7 +215,7 @@ int     setpgrp (void);
 #if defined(__CYGWIN__) && __BSD_VISIBLE
 /* Stub for Linux libbsd compatibility. */
 #define initsetproctitle(c, a, e) setproctitle_init((c), (a), (e))
-static inline void setproctitle_init (int, char *[], char *[]) {}
+static inline void setproctitle_init (int _c, char *_a[], char *_e[]) {}
 
 void setproctitle (const char *, ...)
 		   _ATTRIBUTE ((__format__ (__printf__, 1, 2)));

cygwin/cygwin

commit 1c530c37fd637ab1b7607b810d0495570932b8e7

newlib: sys/unistd.h: Change inline to __inline
Addresses: https://sourceware.org/pipermail/cygwin-patches/2025q2/013644.html
Fixes: 3e8a7eb1a868 ("sys/unistd.h: fix definition of setproctitle_init")
Reported-by: Brian Inglis <Brian.Inglis@SystematicSW.ab.ca>
Co-authored-by: Corinna Vinschen <corinna@vinschen.de>
Signed-off-by: Takashi Yano <takashi.yano@nifty.ne.jp>

newlib/libc/include/sys/unistd.h
@@ -215,7 +215,7 @@ int     setpgrp (void);
 #if defined(__CYGWIN__) && __BSD_VISIBLE
 /* Stub for Linux libbsd compatibility. */
 #define initsetproctitle(c, a, e) setproctitle_init((c), (a), (e))
-static inline void setproctitle_init (int _c, char *_a[], char *_e[]) {}
+static __inline void setproctitle_init (int _c, char *_a[], char *_e[]) {}
 
 void setproctitle (const char *, ...)
 		   _ATTRIBUTE ((__format__ (__printf__, 1, 2)));