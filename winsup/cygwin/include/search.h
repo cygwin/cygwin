picolibc/picolibc

commit ec98d19a08c2e4678e8a6f40fea0c9bbeaa4a2c7

* wininfo.h (wininfo::timer_active): Delete. (wininfo::itv): Ditto. (wininfo::start_time): Ditto. (wininfo::window_started): Ditto. (wininfo::getitimer): Ditto. (wininfo::setitimer): Ditto. (wininfo::wininfo): Ditto. (wininfo::lock): New method. (wininfo::release): Ditto. * window.cc: Use new lock/acquire wininfo methods throughout. (wininfo::wininfo): Delete (wininfo::getitimer): Ditto. (wininfo::setitimer): Ditto. (getitimer): Ditto. (setitimer): Ditto. (ualarm): Ditto. (alarm): Ditto. (wininfo::lock): Define new function. (wininfo::release): Ditto. (wininfo::process): Delete WM_TIMER handling. * timer.cc (struct timetracker): Delete it, flags.  Add it_interval, interval_us, sleepto_us, running, init_muto(), syncthread, and gettime(). (ttstart): Make NO_COPY. (lock_timer_tracker): New class. (timer_tracker::timer_tracker): Distinguish ttstart case. (timer_tracker::~timer_tracker): New destructor.  Clean out events, and reset magic. (timer_tracker::init_muto): New method. (to_us): Round up as per POSIX. (timer_thread): Reorganize to match timer_tracker::settime and timer_tracker::gettime.  Call sig_send without wait.  Call auto_release. (timer_tracker::settime): Reorganize logic to avoid race.  Call gettime to recover old value. (timer_tracker::gettime): New method. (timer_create): Properly set errno on invalid timerid.  Use new lock_timer_tracker method. (timer_delete): Ditto.  Simplify code slightly. (timer_gettime): New function. (fixup_timers_after_fork): Reinit ttstart. (getitimer): New implementation. (setitimer): Ditto. (ualarm): Ditto. (alarm): Ditto. * cygwin.din: Export timer_gettime. * winsup.h: Remove has has_visible_window_station declaration. * Makefile.in (DLL_OFILES): Add lsearch.o. * cygthread.h (cygthread::notify_detached): New element. (cygthread::cygthread): Take optional fourth argument signifying event to signal on thread completion. * cygthread.cc (cygthread::stub): Signal notify_detached event, if it exists. (cygthread::cygthread): Initializ

winsup/cygwin/include/search.h
@@ -1,0 +1,64 @@
+/*-
+ * Written by J.T. Conklin <jtc@netbsd.org>
+ * Public domain.
+ *
+ *	$NetBSD: search.h,v 1.12 1999/02/22 10:34:28 christos Exp $
+ * $FreeBSD: src/include/search.h,v 1.10 2002/10/16 14:29:23 robert Exp $
+ */
+
+#ifndef _SEARCH_H_
+#define _SEARCH_H_
+
+#include <sys/cdefs.h>
+#include <sys/types.h>
+
+typedef	struct entry
+{
+  char *key;
+  void *data;
+} ENTRY;
+
+typedef	enum
+{
+  FIND, ENTER
+} ACTION;
+
+typedef	enum
+{
+  preorder,
+  postorder,
+  endorder,
+  leaf
+} VISIT;
+
+#ifdef _SEARCH_PRIVATE
+typedef	struct node
+{
+  char *key;
+  struct node *llink, *rlink;
+} node_t;
+
+struct que_elem
+{
+  struct que_elem *next;
+  struct que_elem *prev;
+};
+#endif
+
+__BEGIN_DECLS
+int  hcreate (size_t);
+void  hdestroy (void);
+ENTRY *hsearch (ENTRY, ACTION);
+void *lfind (const void *, const void *, size_t *, size_t,
+	     int (*) (const void *, const void *));
+void *lsearch (const void *, void *, size_t *, size_t,
+	       int (*) (const void *, const void *));
+void *tdelete (const void * __restrict, void ** __restrict,
+	       int (*) (const void *, const void *));
+void *tfind (const void *, void * const *,
+	     int (*) (const void *, const void *));
+void *tsearch (const void *, void **, int (*) (const void *, const void *));
+void  twalk (const void *, void (*) (const void *, VISIT, int));
+__END_DECLS
+
+#endif /* !_SEARCH_H_ */

winsup/cygwin/Makefile.in
@@ -117,7 +117,7 @@ MALLOC_OFILES=@MALLOC_OFILES@
 DLL_IMPORTS:=$(w32api_lib)/libkernel32.a $(w32api_lib)/libadvapi32.a
 
 MT_SAFE_OBJECTS:=
-# Please maintain this list in sorted order, with maximum files per 85 col line
+# Please maintain this list in sorted order, with maximum files per 86 col line
 #
 DLL_OFILES:=assert.o autoload.o bsdlib.o ctype.o cxx.o cygheap.o cygthread.o \
 	cygtls.o dcrt0.o debug.o delqueue.o devices.o dir.o dlfcn.o dll_init.o \
@@ -129,8 +129,8 @@ DLL_OFILES:=assert.o autoload.o bsdlib.o ctype.o cxx.o cygheap.o cygthread.o \
 	fhandler_socket.o fhandler_tape.o fhandler_termios.o \
 	fhandler_tty.o fhandler_virtual.o fhandler_windows.o fhandler_zero.o \
 	flock.o fnmatch.o fork.o getopt.o glob.o grp.o heap.o hookapi.o \
-	init.o ioctl.o ipc.o iruserok.o localtime.o malloc_wrapper.o miscfuncs.o \
-	mmap.o msg.o net.o netdb.o ntea.o passwd.o path.o pinfo.o pipe.o \
+	init.o ioctl.o ipc.o iruserok.o localtime.o lsearch.o malloc_wrapper.o \
+	miscfuncs.o mmap.o msg.o net.o netdb.o ntea.o passwd.o path.o pinfo.o pipe.o \
 	poll.o pthread.o regcomp.o regerror.o regexec.o regfree.o \
 	registry.o resource.o scandir.o sched.o sec_acl.o sec_helper.o security.o \
 	select.o sem.o shared.o shm.o sigfe.o signal.o sigproc.o smallprint.o \

winsup/cygwin/cygthread.cc
@@ -26,7 +26,7 @@ DWORD NO_COPY cygthread::main_thread_id;
 bool NO_COPY cygthread::exiting;
 
 /* Initial stub called by cygthread constructor. Performs initial
-   per-thread initialization and loops waiting for new thread functions
+   per-thread initialization and loops waiting for another thread function
    to execute.  */
 DWORD WINAPI
 cygthread::stub (VOID *arg)
@@ -73,10 +73,11 @@ cygthread::stub (VOID *arg)
 	  info->func (info->arg == cygself ? info : info->arg);
 	  /* ...so the above should always return */
 
+	  HANDLE notify = info->notify_detached;
 	  /* If func is NULL, the above function has set that to indicate
 	     that it doesn't want to alert anyone with a SetEvent and should
 	     just be marked as no longer inuse.  Hopefully the function knows
-	     that it is doing.  */
+	     what it is doing.  */
 	  if (!info->func)
 	    info->release (false);
 	  else
@@ -88,6 +89,8 @@ cygthread::stub (VOID *arg)
 	      info->__name = NULL;
 	      SetEvent (info->ev);
 	    }
+	  if (notify)
+	    SetEvent (notify);
 	}
       switch (WaitForSingleObject (info->thread_sync, INFINITE))
 	{
@@ -161,8 +164,8 @@ out:
 }
 
 cygthread::cygthread (LPTHREAD_START_ROUTINE start, LPVOID param,
-		      const char *name): __name (name),
-					 func (start), arg (param)
+		      const char *name, HANDLE notify)
+ : __name (name), func (start), arg (param), notify_detached (notify)
 {
   thread_printf ("name %s, id %p", name, id);
   if (h)
@@ -310,7 +313,9 @@ cygthread::detach (HANDLE sigwait)
       DWORD res;
 
       if (!sigwait)
-	res = WaitForSingleObject (*this, INFINITE);
+	/* If the caller specified a special handle for notification, wait for that.
+	   This assumes that the thread in question is auto releasing. */
+	res = WaitForSingleObject (notify_detached ?: *this, INFINITE);
       else
 	{
 	  /* Lower our priority and give priority to the read thread */

winsup/cygwin/cygthread.h
@@ -26,6 +26,7 @@ class cygthread
   VOID *arg;
   bool is_freerange;
   static bool exiting;
+  HANDLE notify_detached;
  public:
   bool terminate_thread ();
   static DWORD WINAPI stub (VOID *);
@@ -34,7 +35,7 @@ class cygthread
   static const char * name (DWORD = 0);
   void auto_release () {func = NULL;}
   void release (bool);
-  cygthread (LPTHREAD_START_ROUTINE, LPVOID, const char *);
+  cygthread (LPTHREAD_START_ROUTINE, LPVOID, const char *, HANDLE = NULL);
   cygthread () {};
   static void init ();
   bool detach (HANDLE = NULL);

winsup/cygwin/cygtls.cc
@@ -1,6 +1,6 @@
 /* cygtls.cc
 
-   Copyright 2003, 2004 Red Hat, Inc.
+   Copyright 2003, 2004, 2005 Red Hat, Inc.
 
 This software is a copyrighted work licensed under the terms of the
 Cygwin license.  Please consult the file "CYGWIN_LICENSE" for

winsup/cygwin/cygwin.din
@@ -821,6 +821,7 @@ ldexpf NOSIGFE
 _ldexpf = ldexpf NOSIGFE
 ldiv NOSIGFE
 _ldiv = ldiv NOSIGFE
+lfind NOSIGFE
 lgamma NOSIGFE
 _lgamma = lgamma NOSIGFE
 lgamma_r NOSIGFE
@@ -866,6 +867,7 @@ lrint NOSIGFE
 lrintf NOSIGFE
 lround NOSIGFE
 lroundf NOSIGFE
+lsearch NOSIGFE
 lseek SIGFE
 _lseek = lseek SIGFE
 _lseek64 = lseek64 SIGFE
@@ -1436,6 +1438,7 @@ time SIGFE
 _time = time SIGFE
 timer_create SIGFE
 timer_delete SIGFE
+timer_gettime SIGFE
 timer_settime SIGFE
 times SIGFE
 _times = times SIGFE

winsup/cygwin/debug.cc
@@ -1,6 +1,6 @@
 /* debug.cc
 
-   Copyright 1998, 1999, 2000, 2001, 2002, 2003, 2004 Red Hat, Inc.
+   Copyright 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005 Red Hat, Inc.
 
 This software is a copyrighted work licensed under the terms of the
 Cygwin license.  Please consult the file "CYGWIN_LICENSE" for

winsup/cygwin/exceptions.cc
@@ -10,6 +10,8 @@ Cygwin license.  Please consult the file "CYGWIN_LICENSE" for
 details. */
 
 #include "winsup.h"
+#include <wingdi.h>
+#include <winuser.h>
 #include <imagehlp.h>
 #include <stdlib.h>
 #include <setjmp.h>
@@ -780,6 +782,27 @@ out:
     }
   sigproc_printf ("signal %d %sdelivered", sig, interrupted ? "" : "not ");
   return interrupted;
+}
+
+static inline bool
+has_visible_window_station ()
+{
+  HWINSTA station_hdl;
+  USEROBJECTFLAGS uof;
+  DWORD len;
+
+  /* Check if the process is associated with a visible window station.
+     These are processes running on the local desktop as well as processes
+     running in terminal server sessions.
+     Processes running in a service session not explicitely associated
+     with the desktop (using the "Allow service to interact with desktop"
+     property) are running in an invisible window station. */
+  if ((station_hdl = GetProcessWindowStation ())
+      && GetUserObjectInformationA (station_hdl, UOI_FLAGS, &uof,
+				    sizeof uof, &len)
+      && (uof.dwFlags & WSF_VISIBLE))
+    return true;
+  return false;
 }
 
 /* Keyboard interrupt handler.  */

cygwin/cygwin

commit e6b915d86ae328550a05f777ca62c3a6a490afaf

Cygwin: Update search.h functions for POSIX.1-2024
Add type posix_tnode.  Change certain uses of "void" to "posix_tnode" in
both the prototypes and definitions of functions associated with <search.h>.

(Necessary changes to Newlib's /libc/include/search.h have already been
submitted in a patch sent to newlib@sourceware.org.)

Reported-by: Collin Funk <collin.funk1@gmail.com>
Addresses: https://cygwin.com/pipermail/cygwin/2025-April/258032.html
Signed-off-by: Mark Geisert <mark@maxrnd.com>
Fixes: ec98d19a08c2 "* wininfo.h (wininfo::timer_active): Delete."

winsup/cygwin/include/search.h
@@ -39,6 +39,8 @@ typedef	struct node
 } node_t;
 #endif
 
+typedef void posix_tnode;
+
 struct hsearch_data
 {
   struct internal_head *htable;
@@ -58,13 +60,13 @@ ENTRY *hsearch (ENTRY, ACTION);
 int hcreate_r (size_t, struct hsearch_data *);
 void hdestroy_r (struct hsearch_data *);
 int hsearch_r (ENTRY, ACTION, ENTRY **, struct hsearch_data *);
-void *tdelete (const void * __restrict, void ** __restrict,
+void *tdelete (const void * __restrict, posix_tnode ** __restrict,
 	       int (*) (const void *, const void *));
 void tdestroy (void *, void (*)(void *));
-void *tfind (const void *, void **,
+posix_tnode *tfind (const void *, posix_tnode *const *,
 	     int (*) (const void *, const void *));
-void *tsearch (const void *, void **, int (*) (const void *, const void *));
-void  twalk (const void *, void (*) (const void *, VISIT, int));
+posix_tnode *tsearch (const void *, posix_tnode **, int (*) (const void *, const void *));
+void  twalk (const posix_tnode *, void (*) (const posix_tnode *, VISIT, int));
 void *lfind (const void *, const void *, size_t *, size_t,
 	     int (*) (const void *, const void *));
 void *lsearch (const void *, void *, size_t *, size_t,