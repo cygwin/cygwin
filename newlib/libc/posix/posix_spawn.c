756669312c9763079b04c72290dadd2e5b7fc507
Jeremy Drake <cygwin@jdrake.com>
2025-06-17 13:40:12 -0700

    posix_spawn: factor out structs to an internal header
    This will allow Cygwin to examine and handle the posix_spawnattr_t and
    posix_spawn_file_actions_t arguments itself.
    
    Signed-off-by: Jeremy Drake <cygwin@jdrake.com>

--- newlib/libc/posix/posix_spawn.c
+++ newlib/libc/posix/posix_spawn.c
@@ -102,55 +102,12 @@ Supporting OS subroutines required: <<_close>>, <<dup2>>, <<_fcntl>>,
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include "posix_spawn.h"
 
 /* Only deal with a pointer to environ, to work around subtle bugs with shared
    libraries and/or small data systems where the user declares his own
    'environ'.  */
 static char ***p_environ = &environ;
-
-struct __posix_spawnattr {
-	short			sa_flags;
-	pid_t			sa_pgroup;
-	struct sched_param	sa_schedparam;
-	int			sa_schedpolicy;
-	sigset_t		sa_sigdefault;
-	sigset_t		sa_sigmask;
-};
-
-struct __posix_spawn_file_actions {
-	STAILQ_HEAD(, __posix_spawn_file_actions_entry) fa_list;
-};
-
-typedef struct __posix_spawn_file_actions_entry {
-	STAILQ_ENTRY(__posix_spawn_file_actions_entry) fae_list;
-	enum {
-		FAE_OPEN,
-		FAE_DUP2,
-		FAE_CLOSE,
-		FAE_CHDIR,
-		FAE_FCHDIR
-	} fae_action;
-
-	int fae_fildes;
-	union {
-		struct {
-			char *path;
-#define fae_path	fae_data.open.path
-			int oflag;
-#define fae_oflag	fae_data.open.oflag
-			mode_t mode;
-#define fae_mode	fae_data.open.mode
-		} open;
-		struct {
-			int newfildes;
-#define fae_newfildes	fae_data.dup2.newfildes
-		} dup2;
-		char *dir;
-#define fae_dir		fae_data.dir
-		int dirfd;
-#define fae_dirfd		fae_data.dirfd
-	} fae_data;
-} posix_spawn_file_actions_entry_t;
 
 /*
  * Spawn routines
