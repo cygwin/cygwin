42d3f122a2ef2d70383f83f43971c7a2dc38d7ed
Radek Bartoň <radek.barton@microsoft.com>
2025-06-21 22:47:58 +0200

    Cygwin: fix syntax error in cpu_relax.h for AArch64
    Signed-off-by: Radek Bartoň <radek.barton@microsoft.com>

--- winsup/testsuite/winsup.api/pthread/cpu_relax.h
+++ winsup/testsuite/winsup.api/pthread/cpu_relax.h
@@ -4,8 +4,8 @@
 #if defined(__x86_64__) || defined(__i386__)  // Check for x86 architectures
    #define CPU_RELAX() __asm__ volatile ("pause" :::)
 #elif defined(__aarch64__) || defined(__arm__)  // Check for ARM architectures
-   #define CPU_RELAX() __asm__ volatile ("dmb ishst \
-                                          yield" :::)
+   #define CPU_RELAX() __asm__ volatile ("dmb ishst\n" \
+                                         "yield" :::)
 #else
    #error unimplemented for this target
 #endif
