picolibc/picolibc

commit 7d260cfac42db6af3a1c8dd5f1f27491d3963371

Cygwin: add transform_chars_af_unix helper
This function is going to be used for transposing sun_path of
abstract sockets.  This also adds a transposition of the NUL
character to tfx_chars since NUL-bytes in abstract socket names
are perfectly valid.

Signed-off-by: Corinna Vinschen <corinna@vinschen.de>

winsup/cygwin/strfuncs.cc
@@ -22,7 +22,7 @@ details. */
    is affected as well, but we can't transform it as long as we accept Win32
    paths as input. */
 static const WCHAR tfx_chars[] = {
-	    0, 0xf000 |   1, 0xf000 |   2, 0xf000 |   3,
+ 0xf000 |   0, 0xf000 |   1, 0xf000 |   2, 0xf000 |   3,
  0xf000 |   4, 0xf000 |   5, 0xf000 |   6, 0xf000 |   7,
  0xf000 |   8, 0xf000 |   9, 0xf000 |  10, 0xf000 |  11,
  0xf000 |  12, 0xf000 |  13, 0xf000 |  14, 0xf000 |  15,
@@ -61,7 +61,7 @@ static const WCHAR tfx_chars[] = {
    converting back space and dot on filesystems only supporting DOS
    filenames. */
 static const WCHAR tfx_rev_chars[] = {
-	    0, 0xf000 |   1, 0xf000 |   2, 0xf000 |   3,
+ 0xf000 |   0, 0xf000 |   1, 0xf000 |   2, 0xf000 |   3,
  0xf000 |   4, 0xf000 |   5, 0xf000 |   6, 0xf000 |   7,
  0xf000 |   8, 0xf000 |   9, 0xf000 |  10, 0xf000 |  11,
  0xf000 |  12, 0xf000 |  13, 0xf000 |  14, 0xf000 |  15,
@@ -101,6 +101,15 @@ transform_chars (PWCHAR path, PWCHAR path_end)
   for (; path <= path_end; ++path)
     if (*path < 128)
       *path = tfx_chars[*path];
+}
+
+PWCHAR
+transform_chars_af_unix (PWCHAR out, const char *path, __socklen_t len)
+{
+  len -= sizeof (__sa_family_t);
+  for (const unsigned char *p = (const unsigned char *) path; len-- > 0; ++p)
+    *out++ = (*p <= 0x7f) ? tfx_chars[*p] : *p;
+  return out;
 }
 
 /* The SJIS, JIS and eucJP conversion in newlib does not use UTF as

winsup/cygwin/miscfuncs.h
@@ -96,6 +96,8 @@ transform_chars (PUNICODE_STRING upath, USHORT start_idx)
 		   upath->Buffer + upath->Length / sizeof (WCHAR) - 1);
 }
 
+PWCHAR transform_chars_af_unix (PWCHAR, const char *, __socklen_t);
+
 /* Memory checking */
 int __reg2 check_invalid_virtual_addr (const void *s, unsigned sz);
 

cygwin/cygwin

commit 61e38a8fe0270797b4c9b9f2e2ce33265d498aac

Cygwin: drop ASCII NUL from invalid DOS filename char transposition table
The mechanism to convert characters invalid in DOS/Windows filenames
to the Unicode private use area starting at 0xf000 accidentally also
converts ASCII NUL to the private use area.  That usually doesn't happen,
but what if the filename contains the 0xf000 character and we want to
convert it back to multibyte?  In this case the 0xf000 becomes the ASCII
NUL again, and the resulting string is cut short and does not match with
the real filename.  This leads to all sorts of problems like the
inability to unlink the file.

Fix this problem by not marking ASCII NUL as transposable character.

This problem has been introduced by adding code to support abstract
sockets which may contain a NUL char as part of the socket name.  By
reverting the ASCII NUL in the transposition tables,
transform_chars_af_unix has to handle ASCII NUL explicitely now.

Reported-by: Christian Franke <Christian.Franke@t-online.de>
Addresses: https://cygwin.com/pipermail/cygwin/2025-June/258367.html
Fixes: 7d260cfac42d ("Cygwin: add transform_chars_af_unix helper")
Signed-off-by: Corinna Vinschen <corinna@vinschen.de>

winsup/cygwin/strfuncs.cc
@@ -23,7 +23,7 @@ details. */
    is affected as well, but we can't transform it as long as we accept Win32
    paths as input. */
 static const WCHAR tfx_chars[] = {
- 0xf000 |   0, 0xf000 |   1, 0xf000 |   2, 0xf000 |   3,
+            0, 0xf000 |   1, 0xf000 |   2, 0xf000 |   3,
  0xf000 |   4, 0xf000 |   5, 0xf000 |   6, 0xf000 |   7,
  0xf000 |   8, 0xf000 |   9, 0xf000 |  10, 0xf000 |  11,
  0xf000 |  12, 0xf000 |  13, 0xf000 |  14, 0xf000 |  15,
@@ -62,7 +62,7 @@ static const WCHAR tfx_chars[] = {
    converting back space and dot on filesystems only supporting DOS
    filenames. */
 static const WCHAR tfx_rev_chars[] = {
- 0xf000 |   0, 0xf000 |   1, 0xf000 |   2, 0xf000 |   3,
+            0, 0xf000 |   1, 0xf000 |   2, 0xf000 |   3,
  0xf000 |   4, 0xf000 |   5, 0xf000 |   6, 0xf000 |   7,
  0xf000 |   8, 0xf000 |   9, 0xf000 |  10, 0xf000 |  11,
  0xf000 |  12, 0xf000 |  13, 0xf000 |  14, 0xf000 |  15,
@@ -109,7 +109,7 @@ transform_chars_af_unix (PWCHAR out, const char *path, __socklen_t len)
 {
   len -= sizeof (__sa_family_t);
   for (const unsigned char *p = (const unsigned char *) path; len-- > 0; ++p)
-    *out++ = (*p <= 0x7f) ? tfx_chars[*p] : *p;
+    *out++ = (*p <= 0x7f) ? (*p == 0) ? 0xf000 : tfx_chars[*p] : *p;
   return out;
 }
 