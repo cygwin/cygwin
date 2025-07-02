picolibc/picolibc

commit 2a3a02a6876422425fc3ca94bffd54103328a457

2007-10-23  Jeff Johnston  <jjohnstn@redhat.com>
        * libc/stdlib/wcstombs_r.c: Add SUSV2 support for calculating
        size if output buffer is NULL.

newlib/libc/stdlib/wcstombs_r.c
@@ -14,20 +14,29 @@ _DEFUN (_wcstombs_r, (reent, s, pwcs, n, state),
   char buff[8];
   int i, num_to_copy;
 
-  while (n > 0)
+  if (s == NULL)
     {
-      int bytes = _wctomb_r (r, buff, *pwcs, state);
-      if (bytes == -1)
-        return -1;
-      num_to_copy = (n > bytes ? bytes : (int)n);
-      for (i = 0; i < num_to_copy; ++i)
-        *ptr++ = buff[i];
-          
-      if (*pwcs == 0x00)
-        return ptr - s - (n >= bytes);
-      ++pwcs;
-      n -= num_to_copy;
+      size_t num_bytes = 0;
+      while (*pwcs != 0)
+         num_bytes += _wctomb_r (r, buff, *pwcs++, state);
+      return num_bytes;
     }
-
-  return max;
+  else
+    {
+      while (n > 0)
+        {
+          int bytes = _wctomb_r (r, buff, *pwcs, state);
+          if (bytes == -1)
+            return -1;
+          num_to_copy = (n > bytes ? bytes : (int)n);
+          for (i = 0; i < num_to_copy; ++i)
+            *ptr++ = buff[i];
+          
+          if (*pwcs == 0x00)
+            return ptr - s - (n >= bytes);
+          ++pwcs;
+          n -= num_to_copy;
+        }
+      return max;
+    }
 } 

cygwin/cygwin

commit fa272e05bbd049bb39420c6c8997700397283100

wcstombs: also call __WCTOMB on terminating NUL if output buffer is NULL
A __WCTOMB call on the terminating NUL may emit more than a NUL byte.
This is the case if the string ends with a lone UTF-16 high surrogate.

Fixes: 2a3a02a68764 ("Add SUSV2 support for calculating size if output buffer is NULL")
Signed-off-by: Christian Franke <christian.franke@t-online.de>

newlib/libc/stdlib/wcstombs_r.c
@@ -17,14 +17,15 @@ _wcstombs_r (struct _reent *r,
   if (s == NULL)
     {
       size_t num_bytes = 0;
-      while (*pwcs != 0)
+      do
 	{
-	  bytes = __WCTOMB (r, buff, *pwcs++, state);
+	  bytes = __WCTOMB (r, buff, *pwcs, state);
 	  if (bytes == -1)
 	    return -1;
 	  num_bytes += bytes;
 	}
-      return num_bytes;
+      while (*pwcs++ != 0x00);
+      return num_bytes - 1;
     }
   else
     {