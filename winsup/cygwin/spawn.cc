picolibc/picolibc

commit 8d8724ee1b5aa6a1b080c76a0dec3678136d4981

Cygwin: pty: Fix Ctrl-C handling further for non-cygwin apps.
- The recent commit: "Cygwin: pty: Fix Ctrl-C handling for non-cygwin
  apps in background." causes the problem that cmd.exe is terminated
  by Ctrl-C even if it is running in pseudo console. This patch fixes
  the issue.

winsup/cygwin/spawn.cc
@@ -575,6 +575,9 @@ child_info_spawn::worker (const char *prog_arg, const char *const *argv,
 	c_flags |= CREATE_NEW_PROCESS_GROUP;
       refresh_cygheap ();
 
+      if (c_flags & CREATE_NEW_PROCESS_GROUP)
+	myself->process_state |= PID_NEW_PG;
+
       if (mode == _P_DETACH)
 	/* all set */;
       else if (mode != _P_OVERLAY || !my_wr_proc_pipe)

winsup/cygwin/fhandler_tty.cc
@@ -2251,7 +2251,41 @@ fhandler_pty_master::write (const void *ptr, size_t len)
 
       if ((ti.c_lflag & ISIG) && memchr (buf, '\003', nlen))
 	{
-	  get_ttyp ()->kill_pgrp (SIGINT);
+	  /* If the process is started with CREATE_NEW_PROCESS_GROUP
+	     flag, Ctrl-C will not be sent to that process. Therefore,
+	     send Ctrl-break event to that process here. */
+	  DWORD wpid = 0;
+	  winpids pids ((DWORD) 0);
+	  for (unsigned i = 0; i < pids.npids; i++)
+	    {
+	      _pinfo *p = pids[i];
+	      if (p->ctty == get_ttyp ()->ntty
+		  && p->pgid == get_ttyp ()->getpgid ()
+		  && (p->process_state & PID_NEW_PG))
+		{
+		  wpid = p->dwProcessId;
+		  break;
+		}
+	    }
+	  pinfo pinfo_resume = pinfo (myself->ppid);
+	  DWORD resume_pid;
+	  if (pinfo_resume)
+	    resume_pid = pinfo_resume->dwProcessId;
+	  else
+	    resume_pid = get_console_process_id (myself->dwProcessId, false);
+	  if (wpid && resume_pid)
+	    {
+	      WaitForSingleObject (pcon_mutex, INFINITE);
+	      FreeConsole ();
+	      AttachConsole (wpid);
+	      /* CTRL_C_EVENT does not work for the process started with
+		 CREATE_NEW_PROCESS_GROUP flag, so send CTRL_BREAK_EVENT
+		 instead. */
+	      GenerateConsoleCtrlEvent (CTRL_BREAK_EVENT, wpid);
+	      FreeConsole ();
+	      AttachConsole (resume_pid);
+	      ReleaseMutex (pcon_mutex);
+	    }
 	  if (!(ti.c_lflag & NOFLSH))
 	    get_ttyp ()->discard_input = true;
 	}

winsup/cygwin/include/sys/cygwin.h
@@ -274,6 +274,8 @@ enum
   PID_NEW	       = 0x01000, /* Available. */
   PID_ALLPIDS	       = 0x02000, /* used by pinfo scanner */
   PID_PROCINFO	       = 0x08000, /* caller just asks for process info */
+  PID_NEW_PG	       = 0x10000, /* Process created with
+				     CREATE_NEW_PROCESS_GROUOP flag */
   PID_EXITED	       = 0x40000000, /* Free entry. */
   PID_REAPED	       = 0x80000000  /* Reaped */
 };

cygwin/cygwin

commit 41b5f280bde34a202fe1c8dff1793b1a8e62d4f7

Cygwin: spawn: apply PID_NEW_PG flag to spawned child
Previously, it was always applied to "myself", which is only appropriate
in the case of _P_OVERLAY (aka exec).  Apply the flag to "myself" only
in that case, and apply it to the new child instead in other cases.
Also, clear the flag from "myself" in the case of a failed exec.

Fixes: 8d8724ee1b5a ("Cygwin: pty: Fix Ctrl-C handling further for non-cygwin apps.")
Signed-off-by: Jeremy Drake <cygwin@jdrake.com>

winsup/cygwin/spawn.cc
@@ -544,9 +544,6 @@ child_info_spawn::worker (const char *prog_arg, const char *const *argv,
 	c_flags |= CREATE_NEW_PROCESS_GROUP;
       refresh_cygheap ();
 
-      if (c_flags & CREATE_NEW_PROCESS_GROUP)
-	InterlockedOr ((LONG *) &myself->process_state, PID_NEW_PG);
-
       if (mode == _P_DETACH)
 	/* all set */;
       else if (mode != _P_OVERLAY || !my_wr_proc_pipe)
@@ -605,7 +602,12 @@ child_info_spawn::worker (const char *prog_arg, const char *const *argv,
       ::cygheap->user.deimpersonate ();
 
       if (!real_path.iscygexec () && mode == _P_OVERLAY)
-	InterlockedOr ((LONG *) &myself->process_state, PID_NOTCYGWIN);
+	{
+	  LONG pidflags = PID_NOTCYGWIN;
+	  if (c_flags & CREATE_NEW_PROCESS_GROUP)
+	    pidflags |= PID_NEW_PG;
+	  InterlockedOr ((LONG *) &myself->process_state, pidflags);
+	}
 
       cygpid = (mode != _P_OVERLAY) ? create_cygwin_pid () : myself->pid;
 
@@ -707,7 +709,8 @@ child_info_spawn::worker (const char *prog_arg, const char *const *argv,
 	      myself->sendsig = myself->exec_sendsig;
 	      myself->exec_sendsig = NULL;
 	    }
-	  InterlockedAnd ((LONG *) &myself->process_state, ~PID_NOTCYGWIN);
+	  InterlockedAnd ((LONG *) &myself->process_state,
+			  ~(PID_NOTCYGWIN|PID_NEW_PG));
 	  /* Reset handle inheritance to default when the execution of a'
 	     non-Cygwin process fails.  Only need to do this for _P_OVERLAY
 	     since the handle will be closed otherwise.  Don't need to do
@@ -769,7 +772,9 @@ child_info_spawn::worker (const char *prog_arg, const char *const *argv,
 	  myself->set_has_pgid_children ();
 	  ProtectHandle (pi.hThread);
 	  pinfo child (cygpid,
-		       PID_IN_USE | (real_path.iscygexec () ? 0 : PID_NOTCYGWIN));
+		       PID_IN_USE |
+		       (real_path.iscygexec () ? 0 : PID_NOTCYGWIN) |
+		       ((c_flags & CREATE_NEW_PROCESS_GROUP) ? PID_NEW_PG : 0));
 	  if (!child)
 	    {
 	      syscall_printf ("pinfo failed");