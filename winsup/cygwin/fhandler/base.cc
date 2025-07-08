cygwin/cygwin

commit 37c49decc835f65af570a45fc6b8b4f9d9b05ba2

Cygwin: open: only fix up cached DOS file attributes for on-disk files
Don't try to change the file attributes for devices, e.g. /dev/null,
this can lead to confusion later.

Addresse: https://cygwin.com/pipermail/cygwin/2025-April/257940.html
Fixes: 2d81f6ebe3dc ("Cygwin: open: always fix up cached DOS file attributes after NtCreateFile")
Reported-by: Bruno Haible <bruno@clisp.org>
Signed-off-by: Takashi Yano <takashi.yano@nifty.ne.jp>

winsup/cygwin/fhandler/base.cc
@@ -720,29 +720,32 @@ fhandler_base::open (int flags, mode_t mode)
 	goto done;
    }
 
-  /* Fix up file attributes, they are desperately needed later.
+  if (get_device () == FH_FS)
+    {
+      /* Fix up file attributes, they are desperately needed later.
 
-     Originally we only did that in the FILE_CREATED case below, but that's
-     insufficient:
+	 Originally we only did that in the FILE_CREATED case below, but that's
+	 insufficient:
 
-     If two threads try to create the same file at the same time, it's
-     possible that path_conv::check returns the file as non-existant, i. e.,
-     pc.file_attributes () returns INVALID_FILE_ATTRIBUTES, 0xffffffff.
-     However, one of the NtCreateFile will beat the other, so only one of
-     them returns with FILE_CREATED.
+	 If two threads try to create the same file at the same time, it's
+	 possible that path_conv::check returns the file as non-existant, i. e.,
+	 pc.file_attributes () returns INVALID_FILE_ATTRIBUTES, 0xffffffff.
+	 However, one of the NtCreateFile will beat the other, so only one of
+	 them returns with FILE_CREATED.
 
-     The other fhandler_base::open() will instead run into the O_TRUNC
-     conditional (further below), blindly check for the SPARSE attribute
-     and remove that bit.  The result is that the attributes will be
-     0xfffffdff, i.e., everything but SPARSE.  Most annoying is that
-     pc.isdir() will return TRUE.  Hilarity ensues.
+	 The other fhandler_base::open() will instead run into the O_TRUNC
+	 conditional (further below), blindly check for the SPARSE attribute
+	 and remove that bit.  The result is that the attributes will be
+	 0xfffffdff, i.e., everything but SPARSE.  Most annoying is that
+	 pc.isdir() will return TRUE.  Hilarity ensues.
 
-     Note that we use a different IO_STATUS_BLOCK, so as not to overwrite
-     io.Information... */
-  if (!NT_SUCCESS (NtQueryInformationFile (fh, &io_bi, &fbi, sizeof fbi,
-					   FileBasicInformation)))
-    fbi.FileAttributes = file_attributes | FILE_ATTRIBUTE_ARCHIVE;
-  pc.file_attributes (fbi.FileAttributes);
+	 Note that we use a different IO_STATUS_BLOCK, so as not to overwrite
+	 io.Information... */
+      if (!NT_SUCCESS (NtQueryInformationFile (fh, &io_bi, &fbi, sizeof fbi,
+					       FileBasicInformation)))
+	fbi.FileAttributes = file_attributes | FILE_ATTRIBUTE_ARCHIVE;
+      pc.file_attributes (fbi.FileAttributes);
+    }
 
   if (io.Information == FILE_CREATED)
     {

winsup/cygwin/release/3.6.2
@@ -10,3 +10,6 @@ Fixes:
 
 - Fix the console states after the console is closed.
   Addresses: https://cygwin.com/pipermail/cygwin/2025-April/257909.html
+
+- Fix setting DOS attributes on devices.
+  Addresse: https://cygwin.com/pipermail/cygwin/2025-April/257940.html

commit c86c08e0ab41ba8d181876a4bdcfa4cd62264827

Cygwin: open: only fix file attributes when trying to create file
Only try to fix the cached DOS attributes if the caller actually
tried to create the file.  Otherwise the fixup code is called even
in cases where we open the file with minimal query access bits and
NtQueryInformationFile() could fail with STATUS_ACCESS_DENIED and
the new cached DOS attributes are wrong again.

Fixes: 37c49decc835f ("Cygwin: open: only fix up cached DOS file attributes for on-disk files")
Signed-off-by: Corinna Vinschen <corinna@vinschen.de>

winsup/cygwin/fhandler/base.cc
@@ -720,9 +720,9 @@ fhandler_base::open (int flags, mode_t mode)
 	goto done;
    }
 
-  if (get_device () == FH_FS)
+  if (get_device () == FH_FS && (flags & O_CREAT))
     {
-      /* Fix up file attributes, they are desperately needed later.
+      /* Fix up file attributes if we just made an attempt to create the file.
 
 	 Originally we only did that in the FILE_CREATED case below, but that's
 	 insufficient:

winsup/cygwin/release/3.6.4
@@ -25,3 +25,6 @@ Fixes:
 - Fix potential crashing a process PID by accessing /proc/PID/maps
   in parallel.
   Addresses: https://cygwin.com/pipermail/cygwin/2025-May/258198.html
+
+- Fix ACL operations on directories.
+  Addresses: https://cygwin.com/pipermail/cygwin/2025-July/258433.html