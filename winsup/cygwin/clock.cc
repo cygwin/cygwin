cygwin/cygwin

commit 2abb929f0ad26dad05cbfde18fa54098dc79bbb7

Cygwin: clocks: Implement CLOCK_TAI
CLOCK_TAI is like CLOCK_REALTIME ignoring leap secs.  Right now,
2025, it has a positive 37 secs offset from CLOCK_REALTIME.

Given the unpredictability of adding leap secs by the IERS
(International Earth Rotation and Reference Systems Service),
we also add a mechanism to read the current leap secs offset from
/usr/share/zoneinfo/leapseconds, part of the tzdata package.

Signed-off-by: Corinna Vinschen <corinna@vinschen.de>

winsup/cygwin/clock.cc
@@ -3,6 +3,7 @@
 #include "pinfo.h"
 #include "clock.h"
 #include "miscfuncs.h"
+#include <stdio.h>
 
 static inline LONGLONG
 system_qpc_tickspersec ()
@@ -41,6 +42,52 @@ clk_realtime_t::init ()
   InterlockedCompareExchange64 (&ticks_per_sec, system_qpc_tickspersec (), 0);
 }
 
+uint16_t clk_tai_t::leap_secs = 0;
+SRWLOCK clk_tai_t::leap_lock = SRWLOCK_INIT;
+
+void inline
+clk_tai_t::init ()
+{
+  if (leap_secs)
+    return;
+
+  AcquireSRWLockExclusive (&leap_lock);
+  if (!leap_secs)
+    {
+      FILE *fp = fopen ("/usr/share/zoneinfo/leapseconds", "r");
+      if (!fp)
+	leap_secs = 37; /* Leap secs since 2017 */
+      else
+	{
+	  char buf[256];
+
+	  leap_secs = 10; /* Leap secs 1972 */
+	  while (fgets (buf, sizeof buf, fp))
+	    {
+	      if (buf[0] != 'L')
+		continue;
+	      if (strlen (buf) < 30)
+		continue;
+	      switch (buf[26])
+		{
+		case '+':
+		  ++leap_secs;
+		  break;
+		case '-':
+		  --leap_secs;
+		  break;
+		default:
+		  break;
+		}
+	    }
+	  fclose (fp);
+	}
+    }
+  ReleaseSRWLockExclusive (&leap_lock);
+
+  InterlockedCompareExchange64 (&ticks_per_sec, system_qpc_tickspersec (), 0);
+}
+
 void inline
 clk_monotonic_t::init ()
 {
@@ -70,6 +117,15 @@ clk_realtime_t::now (clockid_t clockid, struct timespec *ts)
   now.QuadPart -= FACTOR;
   ts->tv_sec = now.QuadPart / NS100PERSEC;
   ts->tv_nsec = (now.QuadPart % NS100PERSEC) * (NSPERSEC/NS100PERSEC);
+  return 0;
+}
+
+int
+clk_tai_t::now (clockid_t clockid, struct timespec *ts)
+{
+  init ();
+  clk_realtime_t::now (clockid, ts);
+  ts->tv_sec += leap_secs;
   return 0;
 }
 
@@ -238,6 +294,7 @@ clk_monotonic_t::resolution (struct timespec *ts)
 
 static clk_realtime_coarse_t clk_realtime_coarse;
 static clk_realtime_t clk_realtime;
+static clk_tai_t clk_tai;
 static clk_process_t clk_process;
 static clk_thread_t clk_thread;
 static clk_monotonic_t clk_monotonic;
@@ -259,6 +316,8 @@ clk_t *cyg_clock[MAX_CLOCKS] =
   &clk_boottime,
   &clk_realtime_alarm,
   &clk_boottime_alarm,
+  NULL,
+  &clk_tai,
 };
 
 clk_t *

winsup/cygwin/local_includes/clock.h
@@ -99,6 +99,7 @@ class clk_t
     now (0, &ts);
     return ts.tv_sec * MSPERSEC + ts.tv_nsec / (NSPERSEC/MSPERSEC);
   }
+  virtual uint16_t get_leap_secs () { return 0; }
 };
 
 class clk_realtime_coarse_t : public clk_t
@@ -109,9 +110,21 @@ class clk_realtime_coarse_t : public clk_t
 class clk_realtime_t : public clk_t
 {
   void init ();
+ protected:
   virtual int now (clockid_t, struct timespec *);
  public:
   virtual void resolution (struct timespec *);
+};
+
+class clk_tai_t : public clk_realtime_t
+{
+  static uint16_t leap_secs;
+  static SRWLOCK leap_lock;
+
+  void init ();
+  virtual int now (clockid_t, struct timespec *);
+public:
+  virtual uint16_t get_leap_secs () { init (); return leap_secs; }
 };
 
 class clk_process_t : public clk_t

winsup/cygwin/posix_timer.cc
@@ -349,17 +349,27 @@ timer_tracker::settime (int flags, const itimerspec *new_value,
 		 / (NSPERSEC / NS100PERSEC);
 	  if (flags & TIMER_ABSTIME)
 	    {
-	      if (clock_id == CLOCK_REALTIME_COARSE
-		  || clock_id == CLOCK_REALTIME
-		  || clock_id == CLOCK_REALTIME_ALARM)
-		DueTime.QuadPart = ts + FACTOR;
-	      else /* non-REALTIME clocks require relative DueTime. */
+	      switch (clock_id)
 		{
+		case CLOCK_REALTIME_COARSE:
+		case CLOCK_REALTIME:
+		case CLOCK_REALTIME_ALARM:
+		case CLOCK_TAI:
+		  DueTime.QuadPart = ts + FACTOR;
+		  /* TAI time is realtime + leap secs.  NT timers are on
+		     realtime.  Subtract leap secs to get the corresponding
+		     real due time.  Leap secs are always 0 unless CLOCK_TAI. */
+		  DueTime.QuadPart -= get_clock (clock_id)->get_leap_secs ()
+				      * NS100PERSEC;
+		  break;
+		default:
+		  /* non-REALTIME clocks require relative DueTime. */
 		  DueTime.QuadPart = get_clock_now () - ts;
 		  /* If the timestamp was earlier than now, compute number
 		     of expirations and offset DueTime to expire immediately. */
 		  if (DueTime.QuadPart >= 0)
 		    DueTime.QuadPart = -1LL;
+		  break;
 		}
 	    }
 	  else

winsup/cygwin/release/3.7.0
@@ -1,0 +1,4 @@
+What's new:
+-----------
+
+- Support for CLOCK_TAI clock and timer.

winsup/cygwin/signal.cc
@@ -112,7 +112,13 @@ clock_nanosleep (clockid_t clk_id, int flags, const struct timespec *rqtp,
 	{
 	case CLOCK_REALTIME_COARSE:
 	case CLOCK_REALTIME:
+	case CLOCK_TAI:
 	  timeout.QuadPart += FACTOR;
+	  /* TAI time is realtime + leap secs.  NT timers are on realtime.
+	     Subtract leap secs to get the corresponding real due time.
+	     Leap secs are always 0 unless CLOCK_TAI. */
+	  timeout.QuadPart -= get_clock (clk_id)->get_leap_secs ()
+			      * NS100PERSEC;
 	  break;
 	default:
 	  /* other clocks need to be handled with a relative timeout */

winsup/cygwin/thread.cc
@@ -2361,7 +2361,12 @@ pthread_convert_abstime (clockid_t clock_id, const struct timespec *abstime,
     {
     case CLOCK_REALTIME_COARSE:
     case CLOCK_REALTIME:
+    case CLOCK_TAI:
       timeout->QuadPart += FACTOR;
+      /* TAI time is realtime + leap secs.  NT timers are on realtime.
+	 Subtract leap secs to get the corresponding real due time.
+	 Leap secs are always 0 unless CLOCK_TAI. */
+      timeout->QuadPart -= get_clock (clock_id)->get_leap_secs () * NS100PERSEC;
       break;
     default:
       /* other clocks must be handled as relative timeout */

commit a0634931f181e9b46818f6fda15d6c28583dccbb

Cygwin: clocks: read leap secs from /etc/leapsecs on older systems
Systems prior to W10 1803 don't have the leapsecs registry key
HKLM\SYSTEM\CurrentControlSet\Control\LeapSecondInformation and
don't handle leap seconds at all.

Given that new leap seconds are a rather seldom, irregular event,
drop reading from /usr/share/zoneinfo/leapseconds.  Just read the
same leap second records from a file /etc/leapsecs as stored in
the LeapSeconds registry value on newer systems instead.

As a sidenote, the code reading from /usr/share/zoneinfo/leapseconds
was wrong anyway, because it didn't take the timestamps into account.
Given IERS publishes new leap seconds about 6 months before they
occur, CLOCK_TAI would have been one second off for up to 6 months.

/etc/leapsecs doesn't exist yet, so we just default to 37 secs.  If
new leap seconds get provided for newer systems, make sure to provide
the /etc/leapsecs file as part of the Cygwin distro with identical
entries.

Fixes: 2abb929f0ad2 ("Cygwin: clocks: Implement CLOCK_TAI")
Signed-off-by: Corinna Vinschen <corinna@vinschen.de>

winsup/cygwin/clock.cc
@@ -1,10 +1,11 @@
 #include "winsup.h"
+#include <unistd.h>
+#include <fcntl.h>
 #include <realtimeapiset.h>
 #include "pinfo.h"
 #include "clock.h"
 #include "miscfuncs.h"
 #include "registry.h"
-#include <stdio.h>
 
 static inline LONGLONG
 system_qpc_tickspersec ()
@@ -66,6 +67,8 @@ struct reg_leap_secs_t
 void inline
 clk_tai_t::init ()
 {
+  size_t size = 0;
+
   /* Avoid a lock/unlock sequence */
   if (leap_secs)
     return;
@@ -91,66 +94,53 @@ clk_tai_t::init ()
      we always ignore the file! */
   if (!reg.error ())
     {
-      size_t size = 0;
-
       if (!NT_SUCCESS (reg.get_binary (L"LeapSeconds", reg_leap_secs,
 				       sizeof reg_leap_secs, size)))
 	{
 	  ReleaseSRWLockExclusive (&leap_lock);
 	  return;
 	}
-
-      size /= sizeof reg_leap_secs[0];
-      for (size_t i = 0; i < size; ++i)
-	{
-	  struct tm tm = { tm_sec: 59,
-			   tm_min: 59,
-			   tm_hour: 23,
-			   tm_mday: reg_leap_secs[i].day,
-			   tm_mon: reg_leap_secs[i].month - 1,
-			   tm_year: reg_leap_secs[i].year - 1900,
-			   tm_wday: 0,
-			   tm_yday: 0,
-			   tm_isdst: 0,
-			   tm_gmtoff: 0,
-			   tm_zone: 0
-			 };
-	  /* Future timestamp?  We're done.  Note that the leap sec is
-	     second 60, therefore <=, not <! */
-	  if (time (NULL) <= timegm (&tm))
-	    break;
-	  leap_secs += reg_leap_secs[i].negative ? -1 : 1;
-	}
     }
+  /* Windows 8.1 and W10 prior to 1803.  When (if) IERS adds new leap secs,
+     we'll create a file /etc/leapsecs in the same format as the aforementioned
+     registry value used on newer OSes. */
   else
     {
-      FILE *fp = fopen ("/usr/share/zoneinfo/leapseconds", "r");
-      if (fp)
-	{
-	  char buf[256];
+      int fd = open ("/etc/leapsecs", O_RDONLY);
 
-	  leap_secs = 10; /* Leap secs 1972 */
-	  while (fgets (buf, sizeof buf, fp))
-	    {
-	      if (buf[0] != 'L')
-		continue;
-	      if (strlen (buf) < 30)
-		continue;
-	      switch (buf[26])
-		{
-		case '+':
-		  ++leap_secs;
-		  break;
-		case '-':
-		  --leap_secs;
-		  break;
-		default:
-		  break;
-		}
-	    }
-	  fclose (fp);
+      if (fd < 0)
+	{
+	  ReleaseSRWLockExclusive (&leap_lock);
+	  return;
 	}
+      size = (size_t) read (fd, reg_leap_secs, sizeof reg_leap_secs);
+      if ((ssize_t) size < 0)
+	size = 0;
+      close (fd);
     }
+
+  size /= sizeof reg_leap_secs[0];
+  for (size_t i = 0; i < size; ++i)
+    {
+      struct tm tm = { tm_sec: 59,
+		       tm_min: 59,
+		       tm_hour: 23,
+		       tm_mday: reg_leap_secs[i].day,
+		       tm_mon: reg_leap_secs[i].month - 1,
+		       tm_year: reg_leap_secs[i].year - 1900,
+		       tm_wday: 0,
+		       tm_yday: 0,
+		       tm_isdst: 0,
+		       tm_gmtoff: 0,
+		       tm_zone: 0
+		     };
+      /* Future timestamp?  We're done.  Note that the leap sec is
+	 second 60, therefore <=, not <! */
+      if (time (NULL) <= timegm (&tm))
+	break;
+      leap_secs += reg_leap_secs[i].negative ? -1 : 1;
+    }
+
   ReleaseSRWLockExclusive (&leap_lock);
 }
 