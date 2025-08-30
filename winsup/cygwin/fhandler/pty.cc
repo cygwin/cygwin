picolibc/picolibc

commit 9677efcf005a76e9bb23693243801df0a9e5398c

Cygwin: pty: Make FLUSHO and Ctrl-O work.
- Previously, FLUSHO feature was implemented incompletely. With
  this patch, FLUSHO and Ctrl-O (VDISCARD) get working.

winsup/cygwin/select.cc
@@ -710,6 +710,11 @@ peek_pipe (select_record *s, bool from_select)
     }
 
 out:
+  if (fh->get_major () == DEV_PTYM_MAJOR)
+    {
+      fhandler_pty_master *fhm = (fhandler_pty_master *) fh;
+      fhm->set_mask_flusho (s->read_ready);
+    }
   h = fh->get_output_handle_cyg ();
   if (s->write_selected && dev != FH_PIPER)
     {

winsup/cygwin/tty.cc
@@ -252,6 +252,7 @@ tty::init ()
   req_xfer_input = false;
   pcon_input_state = to_cyg;
   last_sig = 0;
+  mask_flusho = false;
 }
 
 HANDLE

winsup/cygwin/fhandler.h
@@ -2467,6 +2467,7 @@ public:
   bool to_be_read_from_pcon (void);
   void get_master_thread_param (master_thread_param_t *p);
   void get_master_fwd_thread_param (master_fwd_thread_param_t *p);
+  void set_mask_flusho (bool m) { get_ttyp ()->mask_flusho = m; }
 };
 
 class fhandler_dev_null: public fhandler_base

winsup/cygwin/tty.h
@@ -130,6 +130,7 @@ private:
   bool master_is_running_as_service;
   bool req_xfer_input;
   xfer_dir pcon_input_state;
+  bool mask_flusho;
 
 public:
   HANDLE from_master () const { return _from_master; }

winsup/cygwin/fhandler_tty.cc
@@ -612,8 +612,8 @@ fhandler_pty_master::process_slave_output (char *buf, size_t len, int pktmode_on
 	      rc = -1;
 	      goto out;
 	    }
-	  /* DISCARD (FLUSHO) and tcflush can finish here. */
-	  if ((get_ttyp ()->ti.c_lflag & FLUSHO || !buf))
+	  /* tclush can finish here. */
+	  if (!buf)
 	    goto out;
 
 	  if (is_nonblocking ())
@@ -671,8 +671,9 @@ fhandler_pty_master::process_slave_output (char *buf, size_t len, int pktmode_on
 
       termios_printf ("bytes read %u", n);
 
-      if (get_ttyp ()->ti.c_lflag & FLUSHO || !buf)
-	continue;
+      if (!buf || ((get_ttyp ()->ti.c_lflag & FLUSHO)
+		   && !get_ttyp ()->mask_flusho))
+	continue; /* Discard read data */
 
       memcpy (optr, outbuf, n);
       optr += n;
@@ -691,6 +692,8 @@ fhandler_pty_master::process_slave_output (char *buf, size_t len, int pktmode_on
     }
 
 out:
+  if (buf)
+    set_mask_flusho (false);
   termios_printf ("returning %d", rc);
   return rc;
 }
@@ -2036,7 +2039,7 @@ fhandler_pty_master::write (const void *ptr, size_t len)
 {
   ssize_t ret;
   char *p = (char *) ptr;
-  termios ti = tc ()->ti;
+  termios &ti = tc ()->ti;
 
   bg_check_types bg = bg_check (SIGTTOU);
   if (bg <= bg_eof)
@@ -2193,7 +2196,7 @@ fhandler_pty_master::tcflush (int queue)
 
   if (queue == TCIFLUSH || queue == TCIOFLUSH)
     ret = process_slave_output (NULL, OUT_BUFFER_SIZE, 0);
-  else if (queue == TCIFLUSH || queue == TCIOFLUSH)
+  if (queue == TCOFLUSH || queue == TCIOFLUSH)
     {
       /* do nothing for now. */
     }
@@ -2929,6 +2932,8 @@ fhandler_pty_common::process_opost_output (HANDLE h, const void *ptr,
 {
   ssize_t towrite = len;
   BOOL res = TRUE;
+  if (ttyp->ti.c_lflag & FLUSHO)
+    return res; /* Discard write data */
   while (towrite)
     {
       if (!is_echo)

cygwin/cygwin

commit 7b5fb35e376cfab42335291008758a2a64ff66b1

Cygwin: pty: Fix FLUSHO handling
Previsouly, FLUSHO did not work correctly.
 1) Even when FLUSHO is set, read() returns with undesired data in
    the pipe if select() is called in advance.
 2) FLUSHO is toggled even in the case pseudo console enabled.
Due to these problems, read data in the pty master was partially
lost when Ctrl-O is pressed.

With this patch, the mask_flusho flag, that was introduced by the
commit 9677efcf005a and caused the issue 1), has been dropped and
select() and read() for pty master discards the pipe data instead
if FLUSHO flag is set.  In addition, FLUSHO handling in the case
pseudo console activated is disabled.

Addresses: https://cygwin.com/pipermail/cygwin/2025-August/258717.html
Fixes: 2cab4d0bb4af ("Cygwin: pty, console: Refactor the code processing special keys.")
Fixes: 9677efcf005a ("Cygwin: pty: Make FLUSHO and Ctrl-O work.")
Reported-by: Reported-by: Thomas Wolff <towo@towo.net>
Signed-off-by: Takashi Yano <takashi.yano@nifty.ne.jp>

winsup/cygwin/fhandler/pty.cc
@@ -693,8 +693,7 @@ fhandler_pty_master::process_slave_output (char *buf, size_t len, int pktmode_on
 
       termios_printf ("bytes read %u", n);
 
-      if (!buf || ((get_ttyp ()->ti.c_lflag & FLUSHO)
-		   && !get_ttyp ()->mask_flusho))
+      if (!buf || (get_ttyp ()->ti.c_lflag & FLUSHO))
 	continue; /* Discard read data */
 
       memcpy (optr, outbuf, n);
@@ -714,8 +713,6 @@ fhandler_pty_master::process_slave_output (char *buf, size_t len, int pktmode_on
     }
 
 out:
-  if (buf)
-    set_mask_flusho (false);
   termios_printf ("returning %d", rc);
   return rc;
 }
@@ -2256,7 +2253,6 @@ fhandler_pty_master::write (const void *ptr, size_t len)
 	      nlen--;
 	      i--;
 	    }
-	  process_stop_start (buf[i], get_ttyp ());
 	}
 
       DWORD n;

winsup/cygwin/local_includes/fhandler.h
@@ -2625,7 +2625,6 @@ public:
   }
   void get_master_thread_param (master_thread_param_t *p);
   void get_master_fwd_thread_param (master_fwd_thread_param_t *p);
-  void set_mask_flusho (bool m) { get_ttyp ()->mask_flusho = m; }
   bool need_send_ctrl_c_event ();
 };
 

winsup/cygwin/local_includes/tty.h
@@ -139,7 +139,6 @@ private:
   bool master_is_running_as_service;
   bool req_xfer_input;
   xfer_dir pty_input_state;
-  bool mask_flusho;
   bool discard_input;
   bool stop_fwd_thread;
 

winsup/cygwin/select.cc
@@ -689,6 +689,8 @@ pipe_data_available (int fd, fhandler_base *fh, HANDLE h, int mode)
   return 0;
 }
 
+SRWLOCK ptym_peek_lock = SRWLOCK_INIT;
+
 static int
 peek_pipe (select_record *s, bool from_select)
 {
@@ -730,10 +732,19 @@ peek_pipe (select_record *s, bool from_select)
 	  gotone = s->read_ready = true;
 	  goto out;
 	}
+      if (fh->get_major () == DEV_PTYM_MAJOR)
+	AcquireSRWLockExclusive (&ptym_peek_lock);
       ssize_t n = pipe_data_available (s->fd, fh, h, PDA_READ);
       /* On PTY masters, check if input from the echo pipe is available. */
       if (n == 0 && fh->get_echo_handle ())
 	n = pipe_data_available (s->fd, fh, fh->get_echo_handle (), PDA_READ);
+      if (fh->get_major () == DEV_PTYM_MAJOR)
+	{
+	  fhandler_pty_master *fhm = (fhandler_pty_master *) fh;
+	  while (n > 0 && (fhm->tc ()->ti.c_lflag & FLUSHO))
+	    n = fhm->process_slave_output (NULL, n, 0); /* Discard pipe data */
+	  ReleaseSRWLockExclusive (&ptym_peek_lock);
+	}
 
       if (n == PDA_ERROR)
 	{
@@ -759,11 +770,6 @@ peek_pipe (select_record *s, bool from_select)
     }
 
 out:
-  if (fh->get_major () == DEV_PTYM_MAJOR)
-    {
-      fhandler_pty_master *fhm = (fhandler_pty_master *) fh;
-      fhm->set_mask_flusho (s->read_ready);
-    }
   h = fh->get_output_handle ();
   if (s->write_selected && dev != FH_PIPER)
     {

winsup/cygwin/tty.cc
@@ -253,7 +253,6 @@ tty::init ()
   req_xfer_input = false;
   pty_input_state = to_cyg;
   last_sig = 0;
-  mask_flusho = false;
   discard_input = false;
   stop_fwd_thread = false;
 }