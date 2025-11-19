picolibc/picolibc

commit c019a66c32f8575c85524e4fb889d79093acee56

* globals.cc (exit_status): Add new ES_EXIT_STARTING enum. * dcrt0.cc (cygwin_exit): Set exit_state to ES_EXIT_STARTING prior to calling real exit. * dll_init.cc (dll_list::detach): Remove dll from linked list and call destructors even if exiting.  Don't call __cxa_finalize in exiting case.

winsup/cygwin/dll_init.cc
@@ -164,7 +164,7 @@ void
 dll_list::detach (void *retaddr)
 {
   dll *d;
-  if (!myself || exit_state || !(d = find (retaddr)))
+  if (!myself || !(d = find (retaddr)))
     return;
   if (d->count <= 0)
     system_printf ("WARNING: trying to detach an already detached dll ...");
@@ -172,7 +172,9 @@ dll_list::detach (void *retaddr)
     {
       /* Ensure our exception handler is enabled for destructors */
       exception protect;
-      __cxa_finalize (d);
+      /* Call finalize function if we are not already exiting */
+      if (!exit_state)
+	__cxa_finalize (d);
       d->run_dtors ();
       d->prev->next = d->next;
       if (d->next)

winsup/cygwin/dcrt0.cc
@@ -1127,6 +1127,7 @@ cygwin_atexit (void (*fn) (void))
 extern "C" void
 cygwin_exit (int n)
 {
+  exit_state = ES_EXIT_STARTING;
   exit (n);
 }
 

winsup/cygwin/globals.cc
@@ -30,6 +30,7 @@ int NO_COPY sigExeced;
 enum exit_states
   {
     ES_NOT_EXITING = 0,
+    ES_EXIT_STARTING,
     ES_PROCESS_LOCKED,
     ES_EVENTS_TERMINATE,
     ES_THREADTERM,

cygwin/cygwin

commit 0d2f981940a496a41b0b650a4ba286d9137302f7

Cygwin: dll_init: Always call __cxa_finalize() for DLL_LOAD
For dlopen()'ed DLL, __cxa_finalize() should always be called at dll
detach time. The reason is as follows. In the case that dlopen()'ed
DLL A is dlclose()'ed in the destructor of DLL B, and the destructor
of DLL B is called in exit_state, DLL A will be unloaded by dlclose().
If __cxa_finalize() for DLL A is not called here, the destructor of
DLL A will be called in exit() even though DLL A is already unloaded.
This causes crash at exit(). In this case, __cxa_finalize() should be
called before unloading DLL A even in exit_state.

Addresses: https://cygwin.com/pipermail/cygwin/2025-October/258877.html
Fixes: c019a66c32f8 ("* dll_init.cc (dll_list::detach) ... Don't call __cxa_finalize in exiting case.")
Reported-by: Thomas Huth <th.huth@posteo.eu>
Reviewed-by: Mark Geisert <mark@maxrnd.com>, Jon Turney <jon.turney@dronecode.org.uk>, Corinna Vinschen <corinna-cygwin@cygwin.com>
Signed-off-by: Takashi Yano <takashi.yano@nifty.ne.jp>

winsup/cygwin/dll_init.cc
@@ -584,7 +584,16 @@ dll_list::detach (void *retaddr)
 	  /* Ensure our exception handler is enabled for destructors */
 	  exception protect;
 	  /* Call finalize function if we are not already exiting */
-	  if (!exit_state)
+	  /* For dlopen()'ed DLL, __cxa_finalize() should always be called
+	     at dll detach time. The reason is as follows. In the case that
+	     dlopen()'ed DLL A is dlclose()'ed in the destructor of DLL B,
+	     and the destructor of DLL B is called in exit_state, DLL A will
+	     be unloaded by dlclose(). If __cxa_finalize() for DLL A is not
+	     called here, the destructor of DLL A will be called in exit()
+	     even though DLL A is already unloaded. This causes crash at
+	     exit(). In this case, __cxa_finalize() should be called before
+	     unloading DLL A even in exit_state. */
+	  if (!exit_state || d->type == DLL_LOAD)
 	    __cxa_finalize (d->handle);
 	  d->run_dtors ();
 	}