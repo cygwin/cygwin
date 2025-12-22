picolibc/picolibc

commit dc7b67316d01c77d81ad6561869b9b89527c2ac8

Cygwin: uinfo: prefer token primary group
internal_getlogin overwrites the process token primary group if it
differs from the primary group as stored in the passwd DB.

However, this also overwrites the primary group of the process if
it has been deliberately changed by a former process (e. g., newgrp),
and the current process has a non-Cygwin process as parent.

Our docs claim we restrict overwriting the primary group to local,
non-domain user accounts anyway, and it was actually meant this way.

So check for exactly that before overwriting the primary group
in the token:  It's only allowed if the user is a local account
and the primary group in the token is still the default group
"None".

Fixes: 6cc7c925ce861 ("(internal_getlogin): Give primary group
from user token more weight.")
Signed-off-by: Corinna Vinschen <corinna@vinschen.de>

winsup/cygwin/uinfo.cc
@@ -163,11 +163,20 @@ internal_getlogin (cygheap_user &user)
 
       user.set_name (pwd->pw_name);
       myself->uid = pwd->pw_uid;
-      myself->gid = pwd->pw_gid;
+      myself->gid = pgrp ? pgrp->gr_gid : pwd->pw_gid;
+
       /* If the primary group in the passwd DB is different from the primary
-	 group in the user token, we have to find the SID of that group and
-	 try to override the token primary group. */
-      if (!pgrp || myself->gid != pgrp->gr_gid)
+	 group in the user token, and if the primary group is the default
+	 group of a local user ("None", localized), we have to find the SID
+	 of that group and try to override the token primary group.  Also
+	 makes sure we're not on a domain controller, where account_sid ()
+	 == primary_sid (). */
+      gsid = cygheap->dom.account_sid ();
+      gsid.append (DOMAIN_GROUP_RID_USERS);
+      if (!pgrp
+	  || (myself->gid != pgrp->gr_gid
+	      && cygheap->dom.account_sid () != cygheap->dom.primary_sid ()
+	      && RtlEqualSid (gsid, user.groups.pgsid)))
 	{
 	  if (gsid.getfromgr (grp = internal_getgrgid (pwd->pw_gid, &cldap)))
 	    {

cygwin/cygwin

commit cc5a6627cbb7639cecd5d87fba1bfb61918b8ed5

Cygwin: uinfo: correctly check and override primary group
Commit dc7b67316d01 ("Cygwin: uinfo: prefer token primary group")
broke the code overriding the primary group in two different ways:

- It changed the way myself->gid was set before checking its value.

  Prior to dc7b67316d01, myself->gid was always set to the primary group
  from the passwd entry (pw_gid).  With the patch, it was set to the
  primary group from the Windows user token (token_gid) in the first
  place.

  The following condition checking if pw_gid is different
  from token_gid did so, by checking token_gid against myself->gid,
  rather than against pw_gid.  After dc7b67316d01 this was always
  false and the code block overriding the primary group in Cygwin and
  the Windows user token with pw_gid was never called anymore.

  The solution is obvious: Do not check token_gid against myself->gid,
  but against the desires primary GID value in pw_gid instead.

- The code block overriding the primary group simply assumed that
  myself->gid was already set to pw_gid, but, as outlined above,
  this was not true anymore after dc7b67316d01.

  This is a subtle error, because it leads to having the wrong primary
  GID in `id' output, while the primary group SID in the user token was
  correctly set.  But as soon as you run this under strace or GDB, the
  problem disappears, because the second process tree under GDB or
  strace takes over from the already changed user token.

  The solution is to override myself->gid with pw_gid once more, after
  successfully changing the primary GID to pw_gid.

Fixes: dc7b67316d01 ("Cygwin: uinfo: prefer token primary group")
Addresses: https://cygwin.com/pipermail/cygwin/2025-December/259068.html
Signed-off-by: Corinna Vinschen <corinna@vinschen.de>
(cherry picked from commit 877dd793a94c54e651e9f755656add0e529749cf)

winsup/cygwin/uinfo.cc
@@ -174,7 +174,7 @@ internal_getlogin (cygheap_user &user)
       gsid = cygheap->dom.account_sid ();
       gsid.append (DOMAIN_GROUP_RID_USERS);
       if (!pgrp
-	  || (myself->gid != pgrp->gr_gid
+	  || (pwd->pw_gid != pgrp->gr_gid
 	      && cygheap->dom.account_sid () != cygheap->dom.primary_sid ()
 	      && RtlEqualSid (gsid, user.groups.pgsid)))
 	{
@@ -209,7 +209,10 @@ internal_getlogin (cygheap_user &user)
 			myself->gid = pwd->pw_gid = pgrp->gr_gid;
 		    }
 		  else
-		    user.groups.pgsid = gsid;
+		    {
+		      user.groups.pgsid = gsid;
+		      myself->gid = pwd->pw_gid;
+		    }
 		  clear_procimptoken ();
 		}
 	    }