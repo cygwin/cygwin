picolibc/picolibc

commit 29b7313d2225eb4468170b94f80acc473b06b7ea

* cygheap.h (enum cygheap_pwdgrp::cache_t): Remove. 	(cygheap_pwdgrp::caching): Convert to bool. 	(cygheap_pwdgrp::pwd_cache): Add cygserver member. 	(cygheap_pwdgrp::grp_cache): Ditto. 	(cygheap_pwdgrp::nss_db_caching): Drop. 	(cygheap_pwdgrp::nss_db_full_caching): Drop. 	(cygheap_pwdgrp::nss_cygserver_caching): New method. 	(cygheap_pwdgrp::nss_disable_cygserver_caching): New method. 	* cygserver.h (client_request::request_code_t): Add 	CYGSERVER_REQUEST_PWDGRP. 	* cygserver_pwdgrp.h: New file. 	* cygtls.h (struct _local_storage): Remove pwbuf and grbuf members. 	* grp.cc (pwdgrp::prep_tls_grbuf): Drop. 	(internal_getgrsid): Handle cygserver caching and rearrange to check 	the caches first. 	(internal_getgrnam): Ditto. 	(internal_getgrgid): Ditto. 	(gr_ent::enumerate_caches): Handle cygserver cache. 	* passwd.cc (pwdgrp::prep_tls_pwbuf): Drop. 	(internal_getpwsid): Handle cygserver caching and rearrange to check 	the caches first. 	(internal_getpwnam): Ditto. 	(internal_getpwuid): Ditto. 	(pw_ent::enumerate_caches): Handle cygserver cache. 	* pwdgrp.h (pwdgrp::add_account_from_cygserver): New method declaration. 	(pwdgrp::fetch_account_from_cygserver): New method declaration. 	(pwdgrp::prep_tls_pwbuf): Drop declaration. 	(pwdgrp::prep_tls_grbuf): Drop declaration. 	(pwdgrp::add_user_from_cygserver): New inline methods. 	(pwdgrp::add_group_from_cygserver): New inline methods. 	* tlsoffsets.h: Regenerate. 	* tlsoffsets64.h: Regenerate. 	* uinfo.cc (internal_getlogin): Call internal_getgroups if cygserver 	caching is not available. 	(cygheap_pwdgrp::init): Initialize pwd_cache.cygserver and 	grp_cache.cygserver.  Set caching to true. 	(cygheap_pwdgrp::nss_init_line): Drop db_cache handling entirely. 	(pwdgrp::add_account_from_windows): Drop no caching handling. 	(client_request_pwdgrp::client_request_pwdgrp): New method. 	(pwdgrp::fetch_account_from_cygserver): New method. 	(pwdgrp::add_account_from_cygserver): New method.

winsup/cygwin/uinfo.cc
@@ -32,8 +32,8 @@ details. */
 #include "tls_pbuf.h"
 #include "miscfuncs.h"
 #include "ntdll.h"
-
 #include "ldap.h"
+#include "cygserver_pwdgrp.h"
 
 /* Initialize the part of cygheap_user that does not depend on files.
    The information is used in shared.cc for the user shared.
@@ -124,7 +124,7 @@ internal_getlogin (cygheap_user &user)
      and the primary group in the token. */
   pwd = internal_getpwsid (user.sid (), &cldap);
   pgrp = internal_getgrsid (user.groups.pgsid, &cldap);
-  if (cygheap->pg.nss_db_full_caching ())
+  if (!cygheap->pg.nss_cygserver_caching ())
     internal_getgroups (0, NULL, &cldap);
   if (!pwd)
     debug_printf ("user not found in passwd DB");
@@ -567,8 +567,10 @@ pwdgrp::add_line (char *eptr)
 void
 cygheap_pwdgrp::init ()
 {
+  pwd_cache.cygserver.init_pwd ();
   pwd_cache.file.init_pwd ();
   pwd_cache.win.init_pwd ();
+  grp_cache.cygserver.init_grp ();
   grp_cache.file.init_grp ();
   grp_cache.win.init_grp ();
   /* Default settings:
@@ -584,7 +586,7 @@ cygheap_pwdgrp::init ()
   grp_src = (NSS_FILES | NSS_DB);
   prefix = NSS_AUTO;
   separator[0] = L'+';
-  caching = NSS_FULL_CACHING;
+  caching = true;
   enums = (ENUM_CACHE | ENUM_BUILTIN);
   enum_tdoms = NULL;
 }
@@ -667,19 +669,6 @@ cygheap_pwdgrp::nss_init_line (const char *line)
 	  c += strspn (c, " \t");
 	  if ((unsigned char) *c <= 0x7f && *c != ':' && strchr (" \t", c[1]))
 	    separator[0] = (unsigned char) *c;
-	  else
-	    debug_printf ("Invalid nsswitch.conf content: %s", line);
-	}
-      else if (!strncmp (c, "cache:", 6))
-	{
-	  c += 6;
-	  c += strspn (c, " \t");
-	  if (!strncmp (c, "full", 3) && strchr (" \t", c[3]))
-	    caching = NSS_FULL_CACHING;
-	  else if (!strncmp (c, "yes", 3) && strchr (" \t", c[3]))
-	    caching = NSS_CACHING;
-	  else if (!strncmp (c, "no", 2) && strchr (" \t", c[2]))
-	    caching = NSS_NO_CACHING;
 	  else
 	    debug_printf ("Invalid nsswitch.conf content: %s", line);
 	}
@@ -1001,11 +990,7 @@ pwdgrp::add_account_from_windows (cygpsid &sid, cyg_ldap *pldap)
   char *line = fetch_account_from_windows (arg, pldap);
   if (!line)
     return NULL;
-  if (cygheap->pg.nss_db_caching ())
-    return add_account_post_fetch (line, true);
-  if (is_group ())
-    return (prep_tls_grbuf ())->add_account_post_fetch (line, false);
-  return (prep_tls_pwbuf ())->add_account_post_fetch (line, false);
+  return add_account_post_fetch (line, true);
 }
 
 void *
@@ -1017,11 +1002,7 @@ pwdgrp::add_account_from_windows (const char *name, cyg_ldap *pldap)
   char *line = fetch_account_from_windows (arg, pldap);
   if (!line)
     return NULL;
-  if (cygheap->pg.nss_db_caching ())
-    return add_account_post_fetch (line, true);
-  if (is_group ())
-    return (prep_tls_grbuf ())->add_account_post_fetch (line, false);
-  return (prep_tls_pwbuf ())->add_account_post_fetch (line, false);
+  return add_account_post_fetch (line, true);
 }
 
 void *
@@ -1033,11 +1014,7 @@ pwdgrp::add_account_from_windows (uint32_t id, cyg_ldap *pldap)
   char *line = fetch_account_from_windows (arg, pldap);
   if (!line)
     return NULL;
-  if (cygheap->pg.nss_db_caching ())
-    return add_account_post_fetch (line, true);
-  if (is_group ())
-    return (prep_tls_grbuf ())->add_account_post_fetch (line, false);
-  return (prep_tls_pwbuf ())->add_account_post_fetch (line, false);
+  return add_account_post_fetch (line, true);
 }
 
 /* Check if file exists and if it has been written to since last checked.
@@ -1827,4 +1804,80 @@ pwdgrp::fetch_account_from_windows (fetch_user_arg_t &arg, cyg_ldap *pldap)
   sys_wcstombs_alloc (&line, HEAP_BUF, linebuf);
   debug_printf ("line: <%s>", line);
   return line;
+}
+
+client_request_pwdgrp::client_request_pwdgrp (fetch_user_arg_t &arg, bool group)
+  : client_request (CYGSERVER_REQUEST_PWDGRP, &_parameters, sizeof (_parameters))
+{
+  size_t len = 0;
+  char *p;
+
+  _parameters.in.group = group;
+  _parameters.in.type = arg.type;
+  switch (arg.type)
+    {
+    case SID_arg:
+      RtlCopySid (sizeof (DBGSID), (PSID) &_parameters.in.arg.sid, *arg.sid);
+      len = RtlLengthSid (*arg.sid);
+      break;
+    case NAME_arg:
+      p = stpcpy (_parameters.in.arg.name, arg.name);
+      len = p - _parameters.in.arg.name;
+      break;
+    case ID_arg:
+      _parameters.in.arg.id = arg.id;
+      len = sizeof (uint32_t);
+    }
+  msglen (__builtin_offsetof (struct _pwdgrp_param_t::_pwdgrp_in_t, arg) + len);
+}
+
+char *
+pwdgrp::fetch_account_from_cygserver (fetch_user_arg_t &arg)
+{
+  client_request_pwdgrp request (arg, is_group ());
+  if (request.make_request () == -1 || request.error_code ())
+    {
+      /* Cygserver not running?  Don't try again.  This will automatically
+	 avoid an endless loop in cygserver itself. */
+      if (request.error_code () == ENOSYS)
+	cygheap->pg.nss_disable_cygserver_caching ();
+      return NULL;
+    }
+  if (!request.line ())
+    return NULL;
+  return cstrdup (request.line ());
+}
+
+void *
+pwdgrp::add_account_from_cygserver (cygpsid &sid)
+{
+  /* No, Everyone is no group in terms of POSIX. */
+  if (sid_id_auth (sid) == 1 /* SECURITY_WORLD_SID_AUTHORITY */
+      && sid_sub_auth (sid, 0) == SECURITY_WORLD_RID)
+    return NULL;
+  fetch_user_arg_t arg;
+  arg.type = SID_arg;
+  arg.sid = &sid;
+  char *line = fetch_account_from_cygserver (arg);
+  return (struct passwd *) add_account_post_fetch (line, true);
+}
+
+void *
+pwdgrp::add_account_from_cygserver (const char *name)
+{
+  fetch_user_arg_t arg;
+  arg.type = NAME_arg;
+  arg.name = name;
+  char *line = fetch_account_from_cygserver (arg);
+  return (struct passwd *) add_account_post_fetch (line, true);
+}
+
+void *
+pwdgrp::add_account_from_cygserver (uint32_t id)
+{
+  fetch_user_arg_t arg;
+  arg.type = ID_arg;
+  arg.id = id;
+  char *line = fetch_account_from_cygserver (arg);
+  return (struct passwd *) add_account_post_fetch (line, true);
 }

winsup/cygwin/cygheap.h
@@ -400,17 +400,12 @@ class cygheap_pwdgrp
     NSS_PRIMARY,
     NSS_ALWAYS
   };
-  enum cache_t {
-    NSS_NO_CACHING = 0,
-    NSS_CACHING,
-    NSS_FULL_CACHING
-  };
   bool    nss_inited;
   int     pwd_src;
   int     grp_src;
   pfx_t   prefix;
   WCHAR   separator[2];
-  cache_t caching;
+  bool    caching;
   int	  enums;
   PWCHAR  enum_tdoms;
 
@@ -419,10 +414,12 @@ class cygheap_pwdgrp
 
 public:
   struct {
+    pwdgrp cygserver;
     pwdgrp file;
     pwdgrp win;
   } pwd_cache;
   struct {
+    pwdgrp cygserver;
     pwdgrp file;
     pwdgrp win;
   } grp_cache;
@@ -438,9 +435,8 @@ public:
   inline bool nss_prefix_primary () const { return prefix == NSS_PRIMARY; }
   inline bool nss_prefix_always () const { return prefix == NSS_ALWAYS; }
   inline PCWSTR nss_separator () const { return separator; }
-  inline bool nss_db_caching () const { return caching != NSS_NO_CACHING; }
-  inline bool nss_db_full_caching () const
-				      { return caching == NSS_FULL_CACHING; }
+  inline bool nss_cygserver_caching () const { return caching; }
+  inline void nss_disable_cygserver_caching () { caching = false; }
   inline int nss_db_enums () const { return enums; }
   inline PCWSTR nss_db_enum_tdoms () const { return enum_tdoms; }
 };

winsup/cygwin/cygserver.h
@@ -52,6 +52,7 @@ protected:
     CYGSERVER_REQUEST_SEM,
     CYGSERVER_REQUEST_SHM,
     CYGSERVER_REQUEST_SETPWD,
+    CYGSERVER_REQUEST_PWDGRP,
     CYGSERVER_REQUEST_LAST
   } request_code_t;
 

winsup/cygwin/cygserver_pwdgrp.h
@@ -1,0 +1,73 @@
+/* cygserver_pwdgrp.h: Request account information
+
+   Copyright 2014 Red Hat, Inc.
+
+This file is part of Cygwin.
+
+This software is a copyrighted work licensed under the terms of the
+Cygwin license.  Please consult the file "CYGWIN_LICENSE" for
+details. */
+
+#ifndef __CYGSERVER_PWDGRP_H__
+#define __CYGSERVER_PWDGRP_H__
+
+#include <sys/types.h>
+#include "cygserver.h"
+
+class transport_layer_base;
+class process_cache;
+
+#ifdef __INSIDE_CYGWIN__
+#include "pwdgrp.h"
+#else
+/* Don't include pwdgrp.h, but keep this in sync. */
+enum fetch_user_arg_type_t {
+  SID_arg,
+  NAME_arg,
+  ID_arg
+};
+#endif
+
+class client_request_pwdgrp : public client_request
+{
+  friend class client_request;
+
+private:
+  union _pwdgrp_param_t
+  {
+    struct _pwdgrp_in_t
+    {
+      bool group;
+      fetch_user_arg_type_t type;
+      union
+      {
+      	BYTE sid[40];
+	char name[UNLEN + 1];
+	uint32_t id;
+      } arg;
+    } in;
+
+    struct
+    {
+      char line[1024];
+    } out;
+  } _parameters;
+
+#ifndef __INSIDE_CYGWIN__
+  client_request_pwdgrp ();
+  virtual void serve (transport_layer_base *, process_cache *);
+  void pwd_serve ();
+  void grp_serve ();
+#endif
+
+public:
+
+#ifdef __INSIDE_CYGWIN__
+  client_request_pwdgrp (fetch_user_arg_t &arg, bool group);
+#endif
+
+  const char *line () const { return (msglen () > 0) ? _parameters.out.line
+						     : NULL; }
+};
+
+#endif /* __CYGSERVER_PWDGRP_H__ */

winsup/cygwin/cygtls.h
@@ -80,11 +80,7 @@ public:
 struct _local_storage
 {
   /* passwd.cc */
-  void *pwbuf;
   char pass[_PASSWORD_LEN];
-
-  /* grp.cc */
-  void *grbuf;
 
   /* dlfcn.cc */
   int dl_error;

winsup/cygwin/fhandler_disk_file.cc
@@ -534,8 +534,7 @@ fhandler_base::fstat_fs (struct stat *buf)
 }
 
 int __reg3
-fhandler_base::fstat_helper (struct stat *buf,
-			     DWORD nNumberOfLinks)
+fhandler_base::fstat_helper (struct stat *buf, DWORD nNumberOfLinks)
 {
   IO_STATUS_BLOCK st;
   FILE_COMPRESSION_INFORMATION fci;

winsup/cygwin/grp.cc
@@ -68,27 +68,6 @@ pwdgrp::init_grp ()
   parse = &pwdgrp::parse_group;
 }
 
-pwdgrp *
-pwdgrp::prep_tls_grbuf ()
-{
-  if (!_my_tls.locals.grbuf)
-    {
-      _my_tls.locals.grbuf = ccalloc_abort (HEAP_BUF, 1,
-					    sizeof (pwdgrp) + sizeof (pg_grp));
-      pwdgrp *gr = (pwdgrp *) _my_tls.locals.grbuf;
-      gr->init_grp ();
-      gr->pwdgrp_buf = (void *) (gr + 1);
-      gr->max_lines = 1;
-    }
-  pwdgrp *gr = (pwdgrp *) _my_tls.locals.grbuf;
-  if (gr->curr_lines)
-    {
-      cfree (gr->group ()[0].g.gr_name);
-      gr->curr_lines = 0;
-    }
-  return gr;
-}         
-
 struct group *
 pwdgrp::find_group (cygpsid &sid)
 {
@@ -122,20 +101,28 @@ internal_getgrsid (cygpsid &sid, cyg_ldap *pldap)
   struct group *ret;
 
   cygheap->pg.nss_init ();
+  /* Check caches first. */
+  if (cygheap->pg.nss_cygserver_caching ()
+      && (ret = cygheap->pg.grp_cache.cygserver.find_group (sid)))
+    return ret;
+  if (cygheap->pg.nss_grp_files ()
+      && (ret = cygheap->pg.grp_cache.file.find_group (sid)))
+    return ret;
+  if (cygheap->pg.nss_grp_db ()
+      && (ret = cygheap->pg.grp_cache.win.find_group (sid)))
+    return ret;
+  /* Ask sources afterwards. */
+  if (cygheap->pg.nss_cygserver_caching ()
+      && (ret = cygheap->pg.grp_cache.cygserver.add_group_from_cygserver (sid)))
+    return ret;
   if (cygheap->pg.nss_grp_files ())
     {
       cygheap->pg.grp_cache.file.check_file ();
-      if ((ret = cygheap->pg.grp_cache.file.find_group (sid)))
-	return ret;
       if ((ret = cygheap->pg.grp_cache.file.add_group_from_file (sid)))
 	return ret;
     }
   if (cygheap->pg.nss_grp_db ())
-    {
-      if ((ret = cygheap->pg.grp_cache.win.find_group (sid)))
-	return ret;
-      return cygheap->pg.grp_cache.win.add_group_from_windows (sid, pldap);
-    }
+    return cygheap->pg.grp_cache.win.add_group_from_windows (sid, pldap);
   return NULL;
 }
 
@@ -153,20 +140,28 @@ internal_getgrnam (const char *name, cyg_ldap *pldap)
   struct group *ret;
 
   cygheap->pg.nss_init ();
+  /* Check caches first. */
+  if (cygheap->pg.nss_cygserver_caching ()
+      && (ret = cygheap->pg.grp_cache.cygserver.find_group (name)))
+    return ret;
+  if (cygheap->pg.nss_grp_files ()
+      && (ret = cygheap->pg.grp_cache.file.find_group (name)))
+    return ret;
+  if (cygheap->pg.nss_grp_db ()
+      && (ret = cygheap->pg.grp_cache.win.find_group (name)))
+    return ret;
+  /* Ask sources afterwards. */
+  if (cygheap->pg.nss_cygserver_caching ()
+      && (ret = cygheap->pg.grp_cache.cygserver.add_group_from_cygserver (name)))
+    return ret;
   if (cygheap->pg.nss_grp_files ())
     {
       cygheap->pg.grp_cache.file.check_file ();
-      if ((ret = cygheap->pg.grp_cache.file.find_group (name)))
-	return ret;
       if ((ret = cygheap->pg.grp_cache.file.add_group_from_file (name)))
 	return ret;
     }
   if (cygheap->pg.nss_grp_db ())
-    {
-      if ((ret = cygheap->pg.grp_cache.win.find_group (name)))
-	return ret;
-      return cygheap->pg.grp_cache.win.add_group_from_windows (name, pldap);
-    }
+    return cygheap->pg.grp_cache.win.add_group_from_windows (name, pldap);
   return NULL;
 }
 
@@ -176,21 +171,27 @@ internal_getgrgid (gid_t gid, cyg_ldap *pldap)
   struct group *ret;
 
   cygheap->pg.nss_init ();
+  /* Check caches first. */
+  if (cygheap->pg.nss_cygserver_caching ()
+      && (ret = cygheap->pg.grp_cache.cygserver.find_group (gid)))
+    return ret;
+  if (cygheap->pg.nss_grp_files ()
+      && (ret = cygheap->pg.grp_cache.file.find_group (gid)))
+    return ret;
+  if (cygheap->pg.nss_grp_db ()
+      && (ret = cygheap->pg.grp_cache.win.find_group (gid)))
+    return ret;
+  /* Ask sources afterwards. */
+  if (cygheap->pg.nss_cygserver_caching ()
+      && (ret = cygheap->pg.grp_cache.cygserver.add_group_from_cygserver (gid)))
+    return ret;
   if (cygheap->pg.nss_grp_files ())
     {
       cygheap->pg.grp_cache.file.check_file ();
-      if ((ret = cygheap->pg.grp_cache.file.find_group (gid)))
-	return ret;
       if ((ret = cygheap->pg.grp_cache.file.add_group_from_file (gid)))
 	return ret;
     }
-  if (cygheap->pg.nss_grp_db ())
-    {
-      if ((ret = cygheap->pg.grp_cache.win.find_group (gid)))
-	return ret;
-      return cygheap->pg.grp_cache.win.add_group_from_windows (gid);
-    }
-  else if (gid == ILLEGAL_GID)
+  if (cygheap->pg.nss_grp_db () || gid == ILLEGAL_GID)
     return cygheap->pg.grp_cache.win.add_group_from_windows (gid, pldap);
   return NULL;
 }
@@ -327,20 +328,37 @@ static gr_ent grent;
 void *
 gr_ent::enumerate_caches ()
 {
-  if (!max && from_files)
+  switch (max)
     {
-      pwdgrp &grf = cygheap->pg.grp_cache.file;
-      grf.check_file ();
-      if (cnt < grf.cached_groups ())
-        return &grf.group ()[cnt++].g;
+    case 0:
+      if (cygheap->pg.nss_cygserver_caching ())
+	{
+	  pwdgrp &grc = cygheap->pg.grp_cache.cygserver;
+	  if (cnt < grc.cached_groups ())
+	    return &grc.group ()[cnt++].g;
+	}
       cnt = 0;
       max = 1;
-    }
-  if (from_db && cygheap->pg.nss_db_caching ())
-    {
-      pwdgrp &grw = cygheap->pg.grp_cache.win;
-      if (cnt < grw.cached_groups ())
-        return &grw.group ()[cnt++].g;
+      /*FALLTHRU*/
+    case 1:
+      if (from_files)
+	{
+	  pwdgrp &grf = cygheap->pg.grp_cache.file;
+	  grf.check_file ();
+	  if (cnt < grf.cached_groups ())
+	    return &grf.group ()[cnt++].g;
+	}
+      cnt = 0;
+      max = 1;
+      /*FALLTHRU*/
+    case 2:
+      if (from_db)
+	{
+	  pwdgrp &grw = cygheap->pg.grp_cache.win;
+	  if (cnt < grw.cached_groups ())
+	    return &grw.group ()[cnt++].g;
+	}
+      break;
     }
   cnt = max = 0;
   return NULL;

winsup/cygwin/include/sys/cygwin.h
@@ -210,8 +210,8 @@ typedef enum
 /* Token type for CW_SET_EXTERNAL_TOKEN */
 enum
 {
-CW_TOKEN_IMPERSONATION = 0,
-CW_TOKEN_RESTRICTED    = 1
+  CW_TOKEN_IMPERSONATION = 0,
+  CW_TOKEN_RESTRICTED    = 1
 };
 
 /* Enumeration source constants for CW_SETENT called from mkpasswd/mkgroup. */

cygwin/cygwin

commit 169a760e5d831f7862ff45812fc0c94fe8df88ec

Cygwin: internal_getlogin: always call internal_getgroups
Commit 29b7313d2225 ("* cygheap.h (enum cygheap_pwdgrp::cache_t):
Remove.") changed an initial conditional to skip calling
internal_getgroups() if we're running with cygserver account caching
in place.  This breaks changing the primary group.

Unfortunately the commit message doesn't explain why the change was
made.

Just calling internal_getgroups() all the time fixes this behaviour.

Fixes: 29b7313d2225 ("* cygheap.h (enum cygheap_pwdgrp::cache_t): Remove.")
Addresses: https://cygwin.com/pipermail/cygwin/2026-January/259250.html
Signed-off-by: Corinna Vinschen <corinna@vinschen.de>

winsup/cygwin/release/3.6.7
@@ -3,3 +3,7 @@ Fixes:
 
 - Guard c32rtomb against invalid input characters.
   Addresses a testsuite error in current gawk git master.
+
+- Allow changing primary group even when running with cygserver account
+  caching.
+  Addresses: https://cygwin.com/pipermail/cygwin/2026-January/259250.html

winsup/cygwin/uinfo.cc
@@ -153,8 +153,7 @@ internal_getlogin (cygheap_user &user)
      and the primary group in the token. */
   pwd = internal_getpwsid (user.sid (), &cldap);
   pgrp = internal_getgrsid (user.groups.pgsid, &cldap);
-  if (!cygheap->pg.nss_cygserver_caching ())
-    internal_getgroups (0, NULL, &cldap);
+  internal_getgroups (0, NULL, &cldap);
   if (!pwd)
     debug_printf ("user not found in passwd DB");
   else