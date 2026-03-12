picolibc/picolibc

commit d584454c8231f5811136b1ab88defe6e5ca81923

* exceptions.cc: (ctrl_c_handler): Do nothing while a Cygwin subprocess is starting. * child_info.h (init_child_info): Remove pid argument from declaration. * cygheap.h (init_cygheap::pid): New element. * dcrt0.cc (dll_crt0_0): Eliminate handling of now-noexistent cygpid parameter in child_info struct.  Set forkee to 'true' rather than cygpid since the pid value was never used. (dll_crt0_1): Ditto. (_dll_crt0): Ditto. * fork.cc (fork_child): Don't wait for sigthread.  This is handled in the fork call now. (fork_parent): Remove obsolete pid argument from init_child_info call.  Don't do anything special with cygpid when DEBUGGING. (fork): Delay all signals during fork. (fork_init): Don't do anything special when DEBUGGING. * pinfo.cc (set_myself): Remove pid parameter.  Use new pid field in cygheap. (pinfo_init): Don't pass pid argument to set_myself. * sigproc.cc (sig_send): Wait for dwProcessId to be non-zero as well as sendsig. (init_child_info): Eliminate handling of pid. (wait_sig): Implement method to temporarily hold off sending signals. * sigproc.h (__SIGHOLD): New enum. (__SIGNOHOLD): Ditto. * spawn.cc (spawn_guts): Remove obsolete pid argument from init_child_info call.

winsup/cygwin/sigproc.cc
@@ -707,6 +707,8 @@ sig_send (_pinfo *p, siginfo_t& si, _cygtls *tls)
     sendsig = myself->sendsig;
   else
     {
+      for (int i = 0; !p->dwProcessId && i < 10000; i++)
+	low_priority_sleep (0);
       HANDLE hp = OpenProcess (PROCESS_DUP_HANDLE, false, p->dwProcessId);
       if (!hp)
 	{
@@ -854,14 +856,13 @@ subproc_init (void)
    by fork/spawn/exec. */
 
 void __stdcall
-init_child_info (DWORD chtype, child_info *ch, pid_t pid, HANDLE subproc_ready)
+init_child_info (DWORD chtype, child_info *ch, HANDLE subproc_ready)
 {
   memset (ch, 0, sizeof *ch);
   ch->cb = chtype == PROC_FORK ? sizeof (child_info_fork) : sizeof (child_info);
   ch->intro = PROC_MAGIC_GENERIC;
   ch->magic = CHILD_INFO_MAGIC;
   ch->type = chtype;
-  ch->cygpid = pid;
   ch->subproc_ready = subproc_ready;
   ch->pppid_handle = myself->ppid_handle;
   ch->fhandler_union_cb = sizeof (fhandler_union);
@@ -1064,6 +1065,7 @@ wait_sig (VOID *self)
 {
   HANDLE readsig;
   char sa_buf[1024];
+  Static bool holding_signals;
 
   /* Initialization */
   (void) SetThreadPriority (GetCurrentThread (), WAIT_SIG_PRIORITY);
@@ -1080,6 +1082,7 @@ wait_sig (VOID *self)
   myself->process_state |= PID_ACTIVE;
   myself->process_state &= ~PID_INITIALIZING;
 
+  sigproc_printf ("myself->dwProcessId %u", myself->dwProcessId);
   /* If we've been execed, then there is still a stub left in the previous
      windows process waiting to see if it's started a cygwin process or not.
      Signalling subproc_ready indicates that we are a cygwin process.  */
@@ -1159,9 +1162,17 @@ wait_sig (VOID *self)
 	    if (q->si.si_signo == __SIGDELETE || q->process () > 0)
 	      sigq.del ();
 	  break;
+	case __SIGHOLD:
+	  holding_signals = 1;
+	  break;
+	case __SIGNOHOLD:
+	  holding_signals = 0;
+	  break;
 	default:
 	  if (pack.si.si_signo < 0)
 	    sig_clear (-pack.si.si_signo);
+	  else if (holding_signals)
+	    sigq.add (pack);
 	  else
 	    {
 	      int sig = pack.si.si_signo;

winsup/cygwin/child_info.h
@@ -42,7 +42,6 @@ public:
   DWORD intro;		// improbable string
   unsigned long magic;	// magic number unique to child_info
   unsigned short type;	// type of record, exec, spawn, fork
-  int cygpid;		// cygwin pid of child process
   HANDLE subproc_ready;	// used for synchronization with parent
   HANDLE user_h;
   HANDLE parent;
@@ -105,7 +104,7 @@ public:
   }
 };
 
-void __stdcall init_child_info (DWORD, child_info *, int, HANDLE);
+void __stdcall init_child_info (DWORD, child_info *, HANDLE);
 
 extern child_info *child_proc_info;
 extern child_info_spawn *spawn_info __attribute__ ((alias ("child_proc_info")));

winsup/cygwin/cygheap.h
@@ -277,6 +277,7 @@ struct init_cygheap
   struct _cygtls **threadlist;
   size_t sthreads;
   int open_fhs;
+  pid_t pid;			/* my pid */
   void close_ctty ();
 };
 

winsup/cygwin/dcrt0.cc
@@ -605,7 +605,6 @@ dll_crt0_0 ()
   GetStartupInfo (&si);
   child_proc_info = (child_info *) si.lpReserved2;
 
-  int mypid = 0;
   if (si.cbReserved2 < EXEC_MAGIC_SIZE || !child_proc_info
       || memcmp (child_proc_info->zero, zeros,
 		 sizeof (child_proc_info->zero)) != 0)
@@ -625,7 +624,7 @@ dll_crt0_0 ()
       switch (child_proc_info->type)
 	{
 	  case _PROC_FORK:
-	    user_data->forkee = child_proc_info->cygpid;
+	    user_data->forkee = true;
 	    should_be_cb = sizeof (child_info_fork);
 	    /* fall through */;
 	  case _PROC_SPAWN:
@@ -639,7 +638,6 @@ dll_crt0_0 ()
 	    else
 	      {
 		cygwin_user_h = child_proc_info->user_h;
-		mypid = child_proc_info->cygpid;
 		break;
 	      }
 	  default:
@@ -667,7 +665,7 @@ dll_crt0_0 ()
 	    alloc_stack (fork_info);
 	    cygheap_fixup_in_child (false);
 	    memory_init ();
-	    set_myself (mypid);
+	    set_myself (NULL);
 	    close_ppid_handle = !!child_proc_info->pppid_handle;
 	    break;
 	  case _PROC_SPAWN:
@@ -686,7 +684,7 @@ dll_crt0_0 ()
 				  hMainProc, &h, 0, FALSE,
 				  DUPLICATE_SAME_ACCESS | DUPLICATE_CLOSE_SOURCE))
 	      h = NULL;
-	    set_myself (mypid, h);
+	    set_myself (h);
 	    __argc = spawn_info->moreinfo->argc;
 	    __argv = spawn_info->moreinfo->argv;
 	    envp = spawn_info->moreinfo->envp;
@@ -771,7 +769,7 @@ dll_crt0_1 (char *)
 	  _tlsbase = (char *) fork_info->stackbottom;
 	  _tlstop = (char *) fork_info->stacktop;
 	}
-      longjmp (fork_info->jmp, fork_info->cygpid);
+      longjmp (fork_info->jmp, true);
     }
 
 #ifdef DEBUGGING
@@ -934,7 +932,7 @@ _dll_crt0 ()
   _impure_ptr->_current_locale = "C";
 
   if (child_proc_info && child_proc_info->type == _PROC_FORK)
-    user_data->forkee = child_proc_info->cygpid;
+    user_data->forkee = true;
   else
     __sinit (_impure_ptr);
 

winsup/cygwin/exceptions.cc
@@ -805,6 +805,8 @@ ctrl_c_handler (DWORD type)
 
   if (!cygwin_finished_initializing)
     {
+      if (myself->ppid_handle)	/* Was this process created by a cygwin process? */
+	return TRUE;		/* Yes.  Let the parent eventually handle CTRL-C issues. */
       debug_printf ("exiting with status %p", STATUS_CONTROL_C_EXIT);
       ExitProcess (STATUS_CONTROL_C_EXIT);
     }

winsup/cygwin/fhandler.cc
@@ -484,10 +484,10 @@ fhandler_base::open_9x (int flags, mode_t mode)
     {
       /* If mode has no write bits set, we set the R/O attribute. */
       if (!(mode & (S_IWUSR | S_IWGRP | S_IWOTH)))
-        file_attributes |= FILE_ATTRIBUTE_READONLY;
+	file_attributes |= FILE_ATTRIBUTE_READONLY;
       /* The file attributes are needed for later use in, e.g. fchmod. */
       pc.file_attributes (file_attributes & FILE_ATTRIBUTE_VALID_SET_FLAGS);
-    } 
+    }
 
   x = CreateFile (get_win32_name (), access, shared, &sa, creation_distribution,
 		  file_attributes, 0);
@@ -818,7 +818,7 @@ fhandler_base::write (const void *ptr, size_t len)
 		 on any OS. */
 	      /* Check there is enough space */
 	      if (!SetEndOfFile (get_output_handle ()))
-	        {
+		{
 		  __seterrno ();
 		  return -1;
 		}
@@ -837,7 +837,7 @@ fhandler_base::write (const void *ptr, size_t len)
 		  if (!ret || written < zeros_this_time)
 		    {
 		      if (!ret)
-		        {
+			{
 			  __seterrno ();
 			  if (get_errno () == EPIPE)
 			    raise (SIGPIPE);

winsup/cygwin/fhandler_dsp.cc
@@ -882,7 +882,7 @@ fhandler_dev_dsp::Audio_in::waitfordata ()
   if (pHdr->dwFlags) /* Zero if queued following error in queueblock */
     {
       /* Errors are ignored here. They will probbaly cause a failure
-         in the subsequent PrepareHeader */
+	 in the subsequent PrepareHeader */
       rc = waveInUnprepareHeader (dev_, pHdr, sizeof (WAVEHDR));
       debug_printf ("%d = waveInUnprepareHeader (0x%08x)", rc, pHdr);
     }
@@ -1149,7 +1149,7 @@ fhandler_dev_dsp::ioctl (unsigned int cmd, void *ptr)
       CASE (SNDCTL_DSP_RESET)
 	close_audio_in ();
 	close_audio_out (true);
-        return 0;
+	return 0;
 	break;
 
       CASE (SNDCTL_DSP_GETBLKSIZE)
@@ -1328,8 +1328,8 @@ fhandler_dev_dsp::ioctl (unsigned int cmd, void *ptr)
       CASE (SNDCTL_DSP_SYNC)
 	// Stop audio out device
 	close_audio_out ();
-        // Stop audio in device
-        close_audio_in ();
+	// Stop audio in device
+	close_audio_in ();
 	return 0;
 
     default:

winsup/cygwin/fhandler_proc.cc
@@ -849,36 +849,36 @@ format_proc_cpuinfo (char *destbuf, size_t maxsize)
 	      if (features1 & (1 << 25))
 		print (" sse");
 	      if (is_intel)
-	        {
-	          if (features1 & (1 << 26))
+		{
+		  if (features1 & (1 << 26))
 		    print (" sse2");
-	          if (features1 & (1 << 27))
+		  if (features1 & (1 << 27))
 		    print (" ss");
-	          if (features1 & (1 << 28))
+		  if (features1 & (1 << 28))
 		    print (" htt");
-	          if (features1 & (1 << 29))
+		  if (features1 & (1 << 29))
 		    print (" tmi");
-	          if (features1 & (1 << 30))
+		  if (features1 & (1 << 30))
 		    print (" ia-64");
-	          if (features1 & (1 << 31))
+		  if (features1 & (1 << 31))
 		    print (" pbe");
 
-	          if (features2 & (1 << 0))
+		  if (features2 & (1 << 0))
 		    print (" pni");
-	          if (features2 & (1 << 3))
+		  if (features2 & (1 << 3))
 		    print (" monitor");
-	          if (features2 & (1 << 4))
+		  if (features2 & (1 << 4))
 		    print (" ds_cpl");
 		  if (features2 & (1 << 7))
 		    print (" tm2");
-	          if (features2 & (1 << 8))
+		  if (features2 & (1 << 8))
 		    print (" est");
-	         if (features2 & (1 << 10))
+		 if (features2 & (1 << 10))
 		    print (" cid");
-	        }
+		}
 
 	      if (is_amd && maxe >= 0x80000001)
-	        {
+		{
 		  // uses AMD extended calls to check
 		  // for 3dnow and 3dnow extended support
 		  // (source: AMD Athlon Processor Recognition Application Note)
@@ -887,14 +887,14 @@ format_proc_cpuinfo (char *destbuf, size_t maxsize)
 		    {
 		      cpuid (&unused, &unused, &unused, &features2, 0x80000001);
 
-                      if (features2 & (1 << 11))
-		        print (" syscall");
-                      if (features2 & (1 << 19))
-		        print (" mp");
-                      if (features2 & (1 << 22))
-                        print (" mmxext");
-                      if (features2 & (1 << 29))
-                        print (" lm");
+		      if (features2 & (1 << 11))
+			print (" syscall");
+		      if (features2 & (1 << 19))
+			print (" mp");
+		      if (features2 & (1 << 22))
+			print (" mmxext");
+		      if (features2 & (1 << 29))
+			print (" lm");
 		      if (features2 & (1 << 30)) // 31th bit is on
 			print (" 3dnowext");
 		      if (features2 & (1 << 31)) // 32th bit (highest) is on

cygwin/cygwin

commit f65c9f0c44444689b732a61ea37589293f70ae64

Cygwin: signal: Wait for `sendsig` for a sufficient amount of time
The current code waits for `sendsig` by `for` loop in sigproc.cc,
however, the wait time might be insufficient for recent CPU.
The current code is as follows.

   for (int i = 0; !p->sendsig && i < 10000; i++)
     yield ();

Due to this problem, in tcsh, the following command occasionally
cannot be terminated by Ctrl-C. This is because, SIGCONT does not
wake-up `sleep` process correctly.

  $ cat | sleep 100 &
  $ fg
  $ (type Ctrl-C)

With this patch, the wait time for `sendsig` is guaranteed to be
up to 100ms instead of looping for 10000 times.

Fixes: d584454c8231 ("* sigproc.cc (sig_send): Wait for dwProcessId to be non-zero as well as sendsig.")
Signed-off-by: Takashi Yano <takashi.yano@nifty.ne.jp>
Reviewed-by: Corinna Vinschen <corinna@vinschen.de>

winsup/cygwin/sigproc.cc
@@ -646,7 +646,8 @@ sig_send (_pinfo *p, siginfo_t& si, _cygtls *tls)
     {
       HANDLE dupsig;
       DWORD dwProcessId;
-      for (int i = 0; !p->sendsig && i < 10000; i++)
+      DWORD t0 = GetTickCount ();
+      while (GetTickCount () - t0 < 100 && !p->sendsig)
 	yield ();
       if (p->sendsig)
 	{