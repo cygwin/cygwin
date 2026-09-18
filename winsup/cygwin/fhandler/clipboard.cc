cygwin/cygwin

commit 7fd670a36b9a26c357f61650a6ec4a0e09936b7c

Cygwin: clipboard: Add workaround for ERROR_CLIPBOARD_NOT_OPEN
SetClipboardData() and GetClipboardData() occasionally fail with
ERROR_CLIPBOARD_NOT_OPEN, even though OpenClipboard() succeeded if
NULL HWND is used. Retry until GetClipboardData() does not return
ERROR_CLIPBOARD_NOT_OPEN.

Addresses: https://cygwin.com/pipermail/cygwin/2026-February/259438.html
Signed-off-by: Takashi Yano <takashi.yano@nifty.ne.jp>
Reviewed-by: Mark Geisert <mark@maxrnd.com>

winsup/cygwin/fhandler/clipboard.cc
@@ -25,11 +25,28 @@ details. */
 static inline bool
 open_clipboard ()
 {
-  const int max_retry = 10;
+  const int max_retry = 20;
   for (int i = 0; i < max_retry; i++)
     {
+      /* No appropriate HWND exists here. */
       if (OpenClipboard (NULL))
-	return true;
+	{
+	  /* SetClipboardData() and GetClipboardData() occasionally
+	     fail with ERROR_CLIPBOARD_NOT_OPEN, even though
+	     OpenClipboard() succeeded if NULL HWND is used.
+	     Retry until GetClipboardData() does not return
+	     ERROR_CLIPBOARD_NOT_OPEN. */
+	  if (GetClipboardData (CF_UNICODETEXT))
+	    return true;
+	  DWORD err = GetLastError ();
+	  /* Here, ERROR_NOT_FOUND means the clipboard does not contains
+	     valid CF_UNICODETEXT. OpenClipboard() must have succeeded. */
+	  if (err == ERROR_NOT_FOUND)
+	    return true;
+	  CloseClipboard ();
+	  if (err != ERROR_CLIPBOARD_NOT_OPEN)
+	    return false;
+	}
       Sleep (1);
     }
   return false;

winsup/cygwin/release/3.6.10
@@ -8,3 +8,6 @@ Fixes:
 
 - Fix behaviour with OPEN_MAX files open
   Addresses: https://cygwin.com/pipermail/cygwin/2026-May/259664.html
+
+- Add workaround for clipboard access error.
+  Addresses: https://cygwin.com/pipermail/cygwin/2026-February/259438.html

commit 8eedf951528285cef63f4c7415e08c1fe0e042b6

Cygwin: clipboard: Handle ERROR_SYS_KEY_NOT_FOUND and NO_ERROR
Windows 10 seems to return ERROR_SXS_KEY_NOT_FOUND instead of
ERROR_NOT_FOUND. In addition, it returns NO_ERROR even though
GetClipboardData() returns NULL. This patch handles these cases.

Addresses: https://cygwin.com/pipermail/cygwin/2026-September/260058.html
Fixes: 7fd670a36b9a ("Cygwin: clipboard: Add workaround for ERROR_CLIPBOARD_NOT_OPEN")
Reported-by: Shun <shun1048576@gmail.com>
Signed-off-by: Takashi Yano <takashi.yano@nifty.ne.jp>
Reviewed-by: Mark Geisert <mark@maxrnd.com>

winsup/cygwin/fhandler/clipboard.cc
@@ -41,7 +41,10 @@ open_clipboard ()
 	  DWORD err = GetLastError ();
 	  /* Here, ERROR_NOT_FOUND means the clipboard does not contains
 	     valid CF_UNICODETEXT. OpenClipboard() must have succeeded. */
-	  if (err == ERROR_NOT_FOUND)
+	  /* In Windows 10, ERROR_SXS_KEY_NOT_FOUND or NO_ERROR seem
+	     to be returned instead of ERROR_NOT_FOUND. */
+	  if (err == ERROR_NOT_FOUND || err == ERROR_SXS_KEY_NOT_FOUND
+	      || err == NO_ERROR)
 	    return true;
 	  CloseClipboard ();
 	  if (err != ERROR_CLIPBOARD_NOT_OPEN)